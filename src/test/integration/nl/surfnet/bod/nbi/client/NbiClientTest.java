ad07335898c690e39d299d7dab9a4ed289be8dbe
robert <robert@localhost>
2011-11-21 12:53:11 +0100

    Added NBI client test. Added test/integration to source paths

---
+++ src/test/integration/nl/surfnet/bod/nbi/client/NbiClientTest.java
@@ -0,0 +1,56 @@
+package nl.surfnet.bod.nbi.client;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import nl.surfnet.bod.nbi.client.generated.TerminationPoint;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/bod-nbi-client.xml" })
+public class NbiClientTest {
+
+	@SuppressWarnings("unused")
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	@Autowired
+	@Qualifier("nbiClient")
+	private NbiClient nbiClient;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetAllPortsWithDetails() {
+		final List<TerminationPoint> allTerminationPoints = nbiClient.getAllPorts();
+		assertEquals(260, allTerminationPoints.size());
+	}
+	
+
+}
