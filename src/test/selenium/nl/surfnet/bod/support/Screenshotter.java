68df386de969e8e1833a2a3a21843e6edc509244
Alan van Dam <avandam@zilverline.com>
2011-11-28 17:02:42 +0100

    removed deprecation warning

--- src/test/selenium/nl/surfnet/bod/support/Screenshotter.java
+++ src/test/selenium/nl/surfnet/bod/support/Screenshotter.java
@@ -2,11 +2,11 @@ package nl.surfnet.bod.support;
 
 import java.io.File;
 
-import org.junit.rules.MethodRule;
-import org.junit.runners.model.FrameworkMethod;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-public class Screenshotter implements MethodRule {
+public class Screenshotter implements TestRule {
     private static final File SCREENSHOTDIR = new File("target/selenium/screenshots");
 
     static {
@@ -20,17 +20,15 @@ public class Screenshotter implements MethodRule {
     }
 
     @Override
-    public Statement apply(final Statement base, final FrameworkMethod method, final Object target) {
+    public Statement apply(final Statement base, final Description description) {
         return new Statement() {
-
             @Override
             public void evaluate() throws Throwable {
                 try {
                     base.evaluate();
-                }
-                catch (Throwable t) {
+                } catch (Throwable t) {
                     t.printStackTrace();
-                    webDriver.takeScreenshot(new File(SCREENSHOTDIR, method.getName() + ".png"));
+                    webDriver.takeScreenshot(new File(SCREENSHOTDIR, description.getMethodName() + ".png"));
                     throw t;
                 }
             }
