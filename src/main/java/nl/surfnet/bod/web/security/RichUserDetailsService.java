2302c87fdc7c74093c95e435123b4a986cd08b69
Alan van Dam <avandam@zilverline.com>
2011-12-19 14:18:26 +0100

    added test for RichUserDetailsService

--- src/main/java/nl/surfnet/bod/web/security/RichUserDetailsService.java
+++ src/main/java/nl/surfnet/bod/web/security/RichUserDetailsService.java
@@ -33,7 +33,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.google.common.base.Predicate;
@@ -42,7 +41,7 @@ import com.google.common.collect.Lists;
 
 public class RichUserDetailsService implements AuthenticationUserDetailsService {
 
-  private static final String NOC_ENGINEER_GROUP = "urn:collab:group:surfteams.nl:nl:surfnet:diensten:noc-engineer";
+  protected static final String NOC_ENGINEER_GROUP_ID = "urn:collab:group:surfteams.nl:nl:surfnet:diensten:noc-engineer";
 
   private final Logger logger = LoggerFactory.getLogger(RichUserDetailsService.class);
 
@@ -50,7 +49,7 @@ public class RichUserDetailsService implements AuthenticationUserDetailsService 
   private GroupService groupService;
 
   @Override
-  public UserDetails loadUserDetails(Authentication token) throws UsernameNotFoundException {
+  public RichUserDetails loadUserDetails(Authentication token) throws UsernameNotFoundException {
     RichPrincipal principal = (RichPrincipal) token.getPrincipal();
 
     Collection<UserGroup> groups = groupService.getGroups(principal.getNameId());
@@ -73,8 +72,12 @@ public class RichUserDetailsService implements AuthenticationUserDetailsService 
     return Iterables.any(groups, new Predicate<UserGroup>() {
       @Override
       public boolean apply(UserGroup group) {
-        return NOC_ENGINEER_GROUP.equals(group.getId());
+        return NOC_ENGINEER_GROUP_ID.equals(group.getId());
       }
     });
+  }
+
+  protected void setGroupService(GroupService groupService) {
+    this.groupService = groupService;
   }
 }
