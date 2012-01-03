ad290786dd135267c37029053c016c08fe144c08
Alan van Dam <avandam@zilverline.com>
2012-01-03 18:05:29 +0100

    added test for manager package...

---
+++ src/test/java/nl/surfnet/bod/web/manager/VirtualPortControllerTest.java
@@ -0,0 +1,61 @@
+/**
+ * The owner of the original code is SURFnet BV.
+ *
+ * Portions created by the original owner are Copyright (C) 2011-2012 the
+ * original owner. All Rights Reserved.
+ *
+ * Portions created by other contributors are Copyright (C) the contributor.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *   (Contributors insert name & email here)
+ *
+ * This file is part of the SURFnet7 Bandwidth on Demand software.
+ *
+ * The SURFnet7 Bandwidth on Demand software is free software: you can
+ * redistribute it and/or modify it under the terms of the BSD license
+ * included with this distribution.
+ *
+ * If the BSD license cannot be found with this distribution, it is available
+ * at the following location <http://www.opensource.org/licenses/BSD-3-Clause>
+ */
+package nl.surfnet.bod.web.manager;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
+import static org.mockito.Mockito.when;
+import nl.surfnet.bod.service.VirtualPortService;
+import nl.surfnet.bod.support.ModelStub;
+import nl.surfnet.bod.support.VirtualPortFactory;
+import nl.surfnet.bod.web.WebUtils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.collect.Lists;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VirtualPortControllerTest {
+
+  @InjectMocks
+  private VirtualPortController subject;
+
+  @Mock
+  private VirtualPortService virtualPortServiceMock;
+
+  @Test
+  public void listShouldFindEntries() {
+    ModelStub model = new ModelStub();
+
+    when(virtualPortServiceMock.findEntries(0, WebUtils.MAX_ITEMS_PER_PAGE)).thenReturn(
+        Lists.newArrayList(new VirtualPortFactory().create()));
+
+    subject.list(0, model);
+
+    assertThat(model.asMap(), hasKey("virtualPortList"));
+  }
+
+}
