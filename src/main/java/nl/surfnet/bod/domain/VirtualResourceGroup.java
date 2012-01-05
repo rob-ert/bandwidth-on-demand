d5e7c5384c4f07877a86d555c831d173eb670f79
Alan van Dam <avandam@zilverline.com>
2012-01-05 14:37:24 +0100

    checkstyle

--- src/main/java/nl/surfnet/bod/domain/VirtualResourceGroup.java
+++ src/main/java/nl/surfnet/bod/domain/VirtualResourceGroup.java
@@ -116,6 +116,7 @@ public class VirtualResourceGroup {
   public String toString() {
     return Objects.toStringHelper(this)
         .add("id", getId())
-        .add("name", getName()).add("surfConextGroupName",surfConextGroupName).toString();
+        .add("name", getName())
+        .add("surfConextGroupName", surfConextGroupName).toString();
   }
 }
