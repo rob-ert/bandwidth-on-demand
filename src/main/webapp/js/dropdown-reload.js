cbceae4dd26baadb1182ffb46c2245b0d6759a53
Alan van Dam <avandam@zilverline.com>
2012-01-11 14:43:30 +0100

    show buttons next to bandwidth in reservation

--- src/main/webapp/js/dropdown-reload.js
+++ src/main/webapp/js/dropdown-reload.js
@@ -1,9 +1,13 @@
 (function($){
-	$.fn.dropdownReload = function(dataUrl, otherSelect) {
+	$.fn.dropdownReload = function(dataUrl, otherSelect, options) {
+		var defaults = {
+		    afterReload: function(data){}
+		};
+		var opts = $.extend(defaults, options);
 		var inputSelect = this;
 		
 		inputSelect.change(function() {
-			$.get(dataUrl.replace("{}", inputSelect.val()), function(responseData) {
+			$.getJSON(dataUrl.replace("{}", inputSelect.val()), function(responseData) {
 				var options = $.map(responseData, function(object, i) {
 					return '<option value="'+object.id+'">'+object.name+'</option>';
 				});
@@ -16,6 +20,7 @@
 				} else {
 					$(otherSelect).html(options.join(""));
 				}
+				opts.afterReload.call(this, responseData);
 			});
 		});
 	};
