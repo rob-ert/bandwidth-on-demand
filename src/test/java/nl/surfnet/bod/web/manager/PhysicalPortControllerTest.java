13d9a4bf7bdba0fbd1544db67765ad784a18480e
Frank Mölder <frank@wesaidso.com>
2012-01-16 15:27:09 +0100

    need to inject service into controller to prevent NPE

--- src/test/java/nl/surfnet/bod/web/manager/PhysicalPortControllerTest.java
+++ src/test/java/nl/surfnet/bod/web/manager/PhysicalPortControllerTest.java
@@ -2,7 +2,6 @@ package nl.surfnet.bod.web.manager;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasKey;
-import nl.surfnet.bod.service.InstituteIddService;
 import nl.surfnet.bod.service.InstituteService;
 import nl.surfnet.bod.service.PhysicalPortService;
 import nl.surfnet.bod.support.ModelStub;
