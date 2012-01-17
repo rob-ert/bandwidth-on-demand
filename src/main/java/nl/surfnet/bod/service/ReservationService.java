573fc23dde6ddf2394288e00e355cd7fd96d4974
Frank Mölder <frank@wesaidso.com>
2012-01-17 16:42:12 +0100

    Corrected import

--- src/main/java/nl/surfnet/bod/service/ReservationService.java
+++ src/main/java/nl/surfnet/bod/service/ReservationService.java
@@ -35,6 +35,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import nl.surfnet.bod.domain.Reservation;
+import nl.surfnet.bod.domain.ReservationStatus;
 import nl.surfnet.bod.repo.ReservationRepo;
 import nl.surfnet.bod.web.security.RichUserDetails;
 import nl.surfnet.bod.web.security.Security;
