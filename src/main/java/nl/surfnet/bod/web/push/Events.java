446ed9a9fff8995fd6a7a301339d92cc84a48637
Frank Mölder <frank@wesaidso.com>
2012-01-26 11:27:51 +0100

    Create a specific reservationStatus changed event

--- src/main/java/nl/surfnet/bod/web/push/Events.java
+++ src/main/java/nl/surfnet/bod/web/push/Events.java
@@ -1,18 +1,40 @@
 package nl.surfnet.bod.web.push;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.surfnet.bod.domain.Reservation;
+import nl.surfnet.bod.domain.ReservationStatus;
+
 public final class Events {
+
+  private Events() {
+
+  }
 
   public static Event createSimpleEvent(String groupId, String message) {
     return new SimpleEvent(groupId, message);
   }
 
+  public static Event createReservationStatusChangedEvent(Reservation reservation, ReservationStatus oldStatus) {
+
+    String message = String.format("The status of Reservation {} , was changed from {} to {}", new Object[] {
+        reservation.getReservationId(), oldStatus, reservation.getStatus() });
+
+    return new SimpleEvent(reservation.getVirtualResourceGroup().getName(), message);
+  }
+
   public static final class SimpleEvent implements Event {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     private String groupId;
     private String message;
 
     public SimpleEvent(String groupId, String message) {
       this.groupId = groupId;
       this.message = message;
+
+      log.debug("Created event for groupId {} with message {}", groupId, message);
     }
 
     @Override
