4c3c7b249fc4796af0873cfa731c131094f8f2dd
Alan van Dam <avandam@zilverline.com>
2012-02-02 15:54:29 +0100

    oeps broke the build, fixed

---
+++ src/test/java/nl/surfnet/bod/service/NbiServiceOpenDracWsTest.java
@@ -0,0 +1,15 @@
+package nl.surfnet.bod.service;
+
+import org.junit.Test;
+
+public class NbiServiceOpenDracWsTest {
+
+  private NbiServiceOpenDracWs subject;
+
+  @Test
+  public void test() {
+    subject.findAllPhysicalPorts();
+
+  }
+
+}
