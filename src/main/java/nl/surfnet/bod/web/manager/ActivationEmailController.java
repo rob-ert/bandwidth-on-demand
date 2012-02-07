050f4bce32da85cac5ea68bda1b0da1a2bbd292e
Alan van Dam <avandam@zilverline.com>
2012-02-07 09:56:00 +0100

    styled the two views confirmed and notValid (email activation)

--- src/main/java/nl/surfnet/bod/web/manager/ActivationEmailController.java
+++ src/main/java/nl/surfnet/bod/web/manager/ActivationEmailController.java
@@ -1,11 +1,14 @@
 package nl.surfnet.bod.web.manager;
 
 import nl.surfnet.bod.domain.ActivationEmailLink;
+import nl.surfnet.bod.domain.PhysicalResourceGroup;
+import nl.surfnet.bod.service.InstituteService;
 import nl.surfnet.bod.service.PhysicalResourceGroupService;
 
 import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,9 +19,11 @@ public class ActivationEmailController {
 
   @Autowired
   private PhysicalResourceGroupService physicalResourceGroupService;
+  @Autowired
+  private InstituteService instituteService;
 
   @RequestMapping(value = "/{uuid}", method = RequestMethod.GET)
-  public String activateEmail(@PathVariable String uuid) {
+  public String activateEmail(@PathVariable String uuid, Model uiModel) {
     ActivationEmailLink link = physicalResourceGroupService.findActivationLink(uuid);
 
     if (link == null) {
@@ -28,7 +33,12 @@ public class ActivationEmailController {
       return "manager/linkNotValid";
     }
 
-    physicalResourceGroupService.activate(link.getPhysicalResourceGroup());
+    PhysicalResourceGroup group = link.getPhysicalResourceGroup();
+
+    physicalResourceGroupService.activate(group);
+    instituteService.fillInstituteForPhysicalResourceGroup(group);
+
+    uiModel.addAttribute("physicalResourceGroup", group);
 
     return "manager/emailConfirmed";
   }
