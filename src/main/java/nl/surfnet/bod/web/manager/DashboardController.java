947cbf2cc7510026ca103b11b63245a7d39d26bd
Alan van Dam <avandam@zilverline.com>
2012-02-08 09:37:51 +0100

    Show flash message when manager has not activated its resource group yet..

--- src/main/java/nl/surfnet/bod/web/manager/DashboardController.java
+++ src/main/java/nl/surfnet/bod/web/manager/DashboardController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.google.common.collect.Lists;
 
 @Controller("managerDashboardController")
 @RequestMapping("/manager")
@@ -20,11 +22,15 @@ public class DashboardController {
   private PhysicalResourceGroupService physicalResourceGroupService;
 
   @RequestMapping(method = RequestMethod.GET)
-  public String index() {
-    Collection<PhysicalResourceGroup> groups = physicalResourceGroupService.findAllForManager(Security.getUserDetails());
+  public String index(RedirectAttributes redirectAttributes) {
+    Collection<PhysicalResourceGroup> groups = physicalResourceGroupService
+        .findAllForManager(Security.getUserDetails());
 
     for (PhysicalResourceGroup group : groups) {
       if (!group.isActive()) {
+        redirectAttributes.addFlashAttribute("infoMessages",
+            Lists.newArrayList("Your Physical Resource group is not activated yet, please do so now."));
+
         return "redirect:manager/physicalresourcegroups/edit?id=" + group.getId();
       }
     }
