e1845d30db1f117ee189ca151903ca79717a7286
Alan van Dam <avandam@zilverline.com>
2012-02-13 11:31:55 +0100

    sending an email for a virtual port request..

--- src/main/java/nl/surfnet/bod/service/EmailSender.java
+++ src/main/java/nl/surfnet/bod/service/EmailSender.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import nl.surfnet.bod.domain.ActivationEmailLink;
 import nl.surfnet.bod.domain.PhysicalResourceGroup;
+import nl.surfnet.bod.domain.VirtualResourceGroup;
 import nl.surfnet.bod.web.manager.ActivationEmailController;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 
 @Service
 public class EmailSender {
@@ -22,28 +26,41 @@ public class EmailSender {
   @Autowired
   private MailSender mailSender;
 
-  public void sendActivationMail(ActivationEmailLink<PhysicalResourceGroup> activationEmailLink) {
-    SimpleMailMessage activationMessage = new SimpleMailMessage();
-    activationMessage.setTo(activationEmailLink.getToEmail());
-    activationMessage.setFrom(fromAddress);
-    activationMessage.setSubject("Activation mail for Physical Resource Group "
-        + activationEmailLink.getSourceObject().getName());
+  private static final String ACTIVATION_BODY =
+      "Please click the link to activate this email adres for physical resource group: %s";
 
-    String body = String.format(
-        "Please click the link to activate this email adres for physical resource group: %s",
+  private static final String VIRTUAL_PORT_REQUEST_BODY =
+      "Dear ICT Manager,\n\n"
+      + "You have received a new Virtual Port Request.\n\n"
+      + "Who: %s\n"
+      + "Reason: %s\n\n"
+      + "Click on the following link %s to create the virtual port";
+
+  public void sendActivationMail(ActivationEmailLink<PhysicalResourceGroup> activationEmailLink) {
+    String bodyText = String.format(ACTIVATION_BODY,
         generateActivationUrl(activationEmailLink).toExternalForm());
 
-    activationMessage.setText(body);
+    SimpleMailMessage mail = new MailMessageBuilder()
+      .withTo(activationEmailLink.getToEmail())
+      .withSubject("Activation mail for Physical Resource Group " + activationEmailLink.getSourceObject().getName())
+      .withBodyText(bodyText)
+      .create();
 
-    mailSender.send(activationMessage);
+    mailSender.send(mail);
   }
 
-  public void sendVirtualPortRequestMail(String to, String from) {
-    SimpleMailMessage mail = new SimpleMailMessage();
-    mail.setTo(to);
-    mail.setReplyTo(from);
-    mail.setFrom(fromAddress);
-    mail.setText("hallo...");
+  public void sendVirtualPortRequestMail(
+      String to, String from, String requestMessage, PhysicalResourceGroup pGroup, VirtualResourceGroup vGroup) {
+    String link = String.format(
+        "http://localhost:8082/bod/manager/virtualports/create?vgroup=%d&pgroup=%d", vGroup.getId(), pGroup.getId());
+
+    SimpleMailMessage mail = new MailMessageBuilder()
+      .withTo(to)
+      .withReplyTo(from)
+      .withSubject("A Virtual Port Request")
+      .withBodyText(String.format(VIRTUAL_PORT_REQUEST_BODY, from, requestMessage, link)).create();
+
+    mailSender.send(mail);
   }
 
   private URL generateActivationUrl(ActivationEmailLink<PhysicalResourceGroup> activationEmailLink) {
@@ -59,6 +76,50 @@ public class EmailSender {
 
   protected void setFromAddress(String fromAddress) {
     this.fromAddress = fromAddress;
+  }
+
+  private final class MailMessageBuilder {
+    private String to;
+    private String subject;
+    private String text;
+    private String replyTo;
+
+    public MailMessageBuilder() {
+    }
+
+    public SimpleMailMessage create() {
+      Preconditions.checkState(Strings.emptyToNull(to) != null);
+      Preconditions.checkState(Strings.emptyToNull(subject) != null);
+      Preconditions.checkState(Strings.emptyToNull(text) != null);
+
+      SimpleMailMessage mailMessage = new SimpleMailMessage();
+      mailMessage.setFrom(fromAddress);
+      mailMessage.setTo(to);
+      mailMessage.setReplyTo(replyTo);
+      mailMessage.setSubject(subject);
+      mailMessage.setText(text);
+      return mailMessage;
+    }
+
+    public MailMessageBuilder withReplyTo(String replyTo) {
+      this.replyTo = replyTo;
+      return this;
+    }
+
+    public MailMessageBuilder withSubject(String subject) {
+      this.subject = subject;
+      return this;
+    }
+
+    public MailMessageBuilder withTo(String to) {
+      this.to = to;
+      return this;
+    }
+
+    public MailMessageBuilder withBodyText(String text) {
+      this.text = text;
+      return this;
+    }
   }
 
 }
