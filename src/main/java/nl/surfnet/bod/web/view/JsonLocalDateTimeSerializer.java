cebb1bfc8ccfe7bc6e0e33bf8908de1cdee69c76
Alan van Dam <avandam@zilverline.com>
2012-02-27 12:49:25 +0100

    checkstyle

--- src/main/java/nl/surfnet/bod/web/view/JsonLocalDateTimeSerializer.java
+++ src/main/java/nl/surfnet/bod/web/view/JsonLocalDateTimeSerializer.java
@@ -3,7 +3,6 @@ package nl.surfnet.bod.web.view;
 import java.io.IOException;
 
 import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.SerializerProvider;
 import org.joda.time.LocalDateTime;
@@ -15,8 +14,7 @@ public class JsonLocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
   private static DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd H:mm");
 
   @Override
-  public void serialize(LocalDateTime value, JsonGenerator jgen, SerializerProvider provider) throws IOException,
-      JsonProcessingException {
+  public void serialize(LocalDateTime value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
     jgen.writeString(formatter.print(value));
   }
 
