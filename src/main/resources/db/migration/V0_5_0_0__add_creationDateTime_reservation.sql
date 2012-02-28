7d7db1171862cdc3cd182f8a22ec720eb6968448
Frank Mölder <frank@wesaidso.com>
2012-02-28 18:00:18 +0100

    Extended script with default value

--- src/main/resources/db/migration/V0_5_0_0__add_creationDateTime_reservation.sql
+++ src/main/resources/db/migration/V0_5_0_0__add_creationDateTime_reservation.sql
@@ -1,1 +1,6 @@
-ALTER TABLE reservation ADD COLUMN creation_date_time TIMESTAMP NOT NULL;
+ALTER TABLE reservation ADD COLUMN creation_date_time TIMESTAMP;
+
+UPDATE reservation res set creation_date_time = (select start_date from reservation where id = res.id);
+
+ALTER TABLE reservation  ALTER COLUMN creation_date_time SET  NOT NULL;
+
