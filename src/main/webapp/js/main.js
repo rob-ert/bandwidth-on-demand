f601ce5c33438f5c845cf1c0c53a4a6adf4376ef
Alan van Dam <avandam@zilverline.com>
2012-02-29 12:25:06 +0100

    make the whole header clickable

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -2,4 +2,16 @@ $(function() {
 	$.fn.twipsy.defaults.delayIn = 500;
 
 	$('[rel="twipsy"]').twipsy();
+
+	$('th.sortable').each(function(i, item) {
+	    var $header = $(item),
+	        $link = $header.find('a'),
+	        url = $link.attr('href');
+
+	    $link.replaceWith($link.html());
+
+	    $header.click(function() {
+	        document.location = url;
+	    });
+	});
 });
