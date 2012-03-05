4a2cc648aee9807a12d3db5256a865202813d5a0
Frank Mölder <frank@wesaidso.com>
2012-03-05 17:46:01 +0100

    Removed log, var is not set in global scope

--- src/main/webapp/js/filter.js
+++ src/main/webapp/js/filter.js
@@ -4,6 +4,5 @@ $(document).ready(function(){
        	var endPos = window.location.href.indexOf("/", startPos+8);
        	var selectedValue = window.location.href.substring(startPos+8, endPos);
        	
-        $("#_filter").val(selectedValue);
-        console.log("filterUrl: " + ${filterUrl});
+        $("#_filter").val(selectedValue);        
 });
