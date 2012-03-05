a071e6a0dc78158532038bf57afe43e00b535fce
Frank Mölder <frank@wesaidso.com>
2012-03-05 17:34:30 +0100

    Moved function to filter.js

---
+++ src/main/webapp/js/filter.js
@@ -0,0 +1,9 @@
+//Sets the filter selectbox to the rest id in the url
+$(document).ready(function(){   	
+       	var startPos = window.location.href.lastIndexOf("/filter/");
+       	var endPos = window.location.href.indexOf("/", startPos+8);
+       	var selectedValue = window.location.href.substring(startPos+8, endPos);
+       	
+        $("#_filter").val(selectedValue);
+        console.log("filterUrl: " + ${filterUrl});
+});
