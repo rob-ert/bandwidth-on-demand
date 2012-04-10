f22aca2c29ac8a0de3a25777bf468f25f3598275
Lars Kappert <lars@webpro.nl>
2012-04-10 17:22:49 +0200

    Added sortable table to styleguide

--- src/main/webapp/js/modules/table.js
+++ src/main/webapp/js/modules/table.js
@@ -10,24 +10,7 @@ app.table = function(){
 
     var initEventHandlers = function() {
 
-        initSortable();
         initRowDetails();
-
-    };
-
-    var initSortable = function() {
-
-        $('th.sortable').each(function(i, item) {
-            var $header = $(item),
-                $link = $header.find('a'),
-                url = $link.attr('href');
-
-            $link.replaceWith($link.html());
-
-            $header.click(function() {
-                document.location = url;
-            });
-        });
 
     };
 
