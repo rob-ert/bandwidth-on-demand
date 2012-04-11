565877434610bd730c1527ff35f49f4839f9befc
Alan van Dam <avandam@zilverline.com>
2012-04-11 17:21:01 +0200

    fixing reflection… (works on test now)

--- src/main/java/nl/surfnet/bod/web/tag/EnvironmentTag.java
+++ src/main/java/nl/surfnet/bod/web/tag/EnvironmentTag.java
@@ -1,14 +1,12 @@
 package nl.surfnet.bod.web.tag;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.PageContext;
 
 import nl.surfnet.bod.util.Environment;
 
-import org.springframework.util.ReflectionUtils;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -19,23 +17,19 @@ public class EnvironmentTag extends BodTagSupport {
 
   @Override
   public void doTag() throws JspException, IOException {
-    Field field = ReflectionUtils.findField(Environment.class, property);
+    Environment env = getEnvironment();
 
-    if (field == null) {
-      throw new JspException(String.format("Could not find property %s in the environment", property));
+    try {
+      Object value = PropertyUtils.getSimpleProperty(env, property);
+      getPageContext().setAttribute(var, value);
     }
-
-    PageContext pageContext = getPageContext();
-
-    Environment env = getEnvironment(pageContext);
-
-    Object value = ReflectionUtils.getField(field, env);
-
-    pageContext.setAttribute(var, value);
+    catch (Exception e) {
+      throw new JspException(e);
+    }
   }
 
-  private Environment getEnvironment(PageContext pageContext) {
-    WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(pageContext
+  private Environment getEnvironment() {
+    WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(getPageContext()
         .getServletContext());
     Environment env = applicationContext.getBean(Environment.class);
     return env;
