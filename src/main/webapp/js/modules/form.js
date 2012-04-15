09dd5ab8344c75eb2ea59120b7a7e2a5a2a903a5
Lars Kappert <lars@webpro.nl>
2012-04-15 14:42:40 +0200

    Moved userSelection() to global.js

--- src/main/webapp/js/modules/form.js
+++ src/main/webapp/js/modules/form.js
@@ -10,32 +10,10 @@ app.form = function(){
 
     var initEventHandlers = function() {
 
-        initUserSelection();
         initFormLinks();
         initBandwidthSelector();
         initDatepickers();
         initStartNow();
-
-    };
-
-    var initUserSelection = function() {
-
-        var form = $('.dropdown-menu');
-
-        form.on('click', 'li', function(event) {
-
-            var item = $(event.target).closest('li')[0];
-            var roleId = item.getAttribute('data-roleId');
-
-            $('<input>').attr({
-                type: 'hidden',
-                name: 'roleId',
-                value: roleId
-            }).appendTo(form);
-
-            form[0].submit();
-
-        })
 
     };
 
--- src/main/webapp/js/modules/global.js
+++ src/main/webapp/js/modules/global.js
@@ -10,8 +10,30 @@ app.global = function() {
 
     var initEventHandlers = function() {
 
+        initUserSelection();
         initTooltips();
         initPopovers();
+
+    };
+
+    var initUserSelection = function() {
+
+        var form = $('.dropdown-menu');
+
+        form.on('click', 'li', function(event) {
+
+            var item = $(event.target).closest('li')[0];
+            var roleId = item.getAttribute('data-roleId');
+
+            $('<input>').attr({
+                type: 'hidden',
+                name: 'roleId',
+                value: roleId
+            }).appendTo(form);
+
+            form[0].submit();
+
+        })
 
     };
 
