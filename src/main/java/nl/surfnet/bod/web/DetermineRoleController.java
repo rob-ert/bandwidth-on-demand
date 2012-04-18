6a2c893e2e0832c6876dcc040cda7f132149ae8f
Frank Mölder <frank@wesaidso.com>
2012-04-18 16:56:41 +0200

    No need to pass a model

--- src/main/java/nl/surfnet/bod/web/DetermineRoleController.java
+++ src/main/java/nl/surfnet/bod/web/DetermineRoleController.java
@@ -4,7 +4,6 @@ import nl.surfnet.bod.web.security.Security;
 import nl.surfnet.bod.web.security.Security.RoleEnum;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class DetermineRoleController {
 
   @RequestMapping(method = RequestMethod.GET)
-  public String index(Model model) {
+  public String index() {
 
     if (Security.isSelectedNocRole()) {
       return RoleEnum.NOC_ENGINEER.getViewName();
