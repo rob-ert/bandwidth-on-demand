acbfd1fe60a186bbfc8c0b1ac398053f103678dd
robert <robert@localhost>
2012-05-07 13:52:49 +0200

    Changed javascript so that it will also update the port id the moment the pp has changed.

--- src/main/webapp/js/modules/form.js
+++ src/main/webapp/js/modules/form.js
@@ -274,7 +274,7 @@ app.form = function(){
         $('[data-component="physicalport-selector"]').on('change', function() {
             var selected = $(this).find('option:selected');
             $("#_nocLabel_id").val(selected.attr("data-noclabel"));
-            $("#_managerLabel_id").val(selected.attr("data-managerlabel"));
+            $("#_portId_id").val(selected.attr("data-portid"));
         });
     }
 
