0ca0d7b883d270d77e677ea68b3bc0acde3b3b76
robert <robert@localhost>
2012-05-25 22:36:43 +0200

    Added start-, end time and duration.

--- src/test/java/nl/surfnet/bod/support/NsiReservationFactory.java
+++ src/test/java/nl/surfnet/bod/support/NsiReservationFactory.java
@@ -1,5 +1,7 @@
 package nl.surfnet.bod.support;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import nl.surfnet.bod.web.services.NsiConnectionService;
@@ -10,10 +12,15 @@ import org.ogf.schemas.nsi._2011._10.connection.types.ReservationInfoType;
 import org.ogf.schemas.nsi._2011._10.connection.types.ReserveType;
 import org.ogf.schemas.nsi._2011._10.connection.types.ScheduleType;
 import org.ogf.schemas.nsi._2011._10.connection.types.ServiceParametersType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NsiReservationFactory {
 
   public static final String NSI_REQUESTER_ENDPOINT = "http://localhost:9082/bod/nsi/requester";
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
   private String correlationId = NsiConnectionService.getCorrelationId();
   private String nsaProviderUrn = "urn:ogf:network:nsa:netherlight";
   private int desiredBandwidth = 1000;
@@ -45,10 +52,23 @@ public class NsiReservationFactory {
     serviceParameters.setBandwidth(bandwidth);
 
     final ScheduleType schedule = new ScheduleType();
-    schedule.setDuration(null);
     schedule.setEndTime(scheduleEndTime);
     schedule.setStartTime(scheduleStartTime);
+
+    if (schedule.getEndTime() != null && schedule.getStartTime() != null) {
+      try {
+        schedule.setDuration(DatatypeFactory.newInstance().newDuration(
+            schedule.getEndTime().getMillisecond() - schedule.getStartTime().getMillisecond()));
+      }
+      catch (DatatypeConfigurationException e) {
+        log.error("Error: ", e);
+      }
+    }
     serviceParameters.setSchedule(schedule);
+    
+    System.out.println(schedule.getStartTime());
+    System.out.println(schedule.getEndTime());
+    System.out.println(schedule.getDuration());
 
     reservationInfoType.setServiceParameters(serviceParameters);
     reservationType.setReservation(reservationInfoType);
@@ -56,12 +76,12 @@ public class NsiReservationFactory {
     return reservationRequest;
   }
 
-  public NsiReservationFactory setCorrelationId(String correlationId) {
+  public final NsiReservationFactory setCorrelationId(String correlationId) {
     this.correlationId = correlationId;
     return this;
   }
 
-  public NsiReservationFactory setNsaProviderUrn(String nsaProviderUrn) {
+  public final NsiReservationFactory setNsaProviderUrn(String nsaProviderUrn) {
     this.nsaProviderUrn = nsaProviderUrn;
     return this;
   }
@@ -78,6 +98,16 @@ public class NsiReservationFactory {
 
   public final NsiReservationFactory setMinBandwidth(int minBandwidth) {
     this.minBandwidth = minBandwidth;
+    return this;
+  }
+
+  public final NsiReservationFactory setScheduleEndTime(XMLGregorianCalendar scheduleEndTime) {
+    this.scheduleEndTime = scheduleEndTime;
+    return this;
+  }
+
+  public final NsiReservationFactory setScheduleStartTime(XMLGregorianCalendar scheduleStartTime) {
+    this.scheduleStartTime = scheduleStartTime;
     return this;
   }
 
--- src/test/java/nl/surfnet/bod/web/services/NsiConnectionServiceProviderTest.java
+++ src/test/java/nl/surfnet/bod/web/services/NsiConnectionServiceProviderTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.util.UUID;
 
 import javax.annotation.Resource;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 import nl.surfnet.bod.support.NsiReservationFactory;
 
@@ -32,13 +34,12 @@ public class NsiConnectionServiceProviderTest extends AbstractTransactionalJUnit
 
   @Resource(name = "nsiProvider")
   private NsiConnectionServiceProvider nsiProvider;
-  
-  
-  private  MockWebServiceServer mockServer;
+
+  @SuppressWarnings("unused")
+  private MockWebServiceServer mockServer;
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
-    
   }
 
   @AfterClass
@@ -49,7 +50,7 @@ public class NsiConnectionServiceProviderTest extends AbstractTransactionalJUnit
   public void setUp() throws Exception {
     final WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
     webServiceTemplate.setDefaultUri(NsiReservationFactory.NSI_REQUESTER_ENDPOINT);
-     mockServer = MockWebServiceServer.createServer(webServiceTemplate);
+    mockServer = MockWebServiceServer.createServer(webServiceTemplate);
   }
 
   @After
@@ -65,21 +66,32 @@ public class NsiConnectionServiceProviderTest extends AbstractTransactionalJUnit
   public void should_throw_exeption_because_of_invalid_provider_urn() throws ServiceException {
     final ReserveRequestType reservationRequest = new NsiReservationFactory().setNsaProviderUrn(
         "urn:ogf:network:nsa:no:such:provider").createReservation();
-    final GenericAcknowledgmentType genericAcknowledgmentType = nsiProvider.reserve(reservationRequest);
-    assertEquals(reservationRequest.getCorrelationId(), genericAcknowledgmentType.getCorrelationId());
+    nsiProvider.reserve(reservationRequest);
   }
 
   @Test(expected = ServiceException.class)
   public void should_throw_exeption_because_of_invalid_correlation_id() throws ServiceException {
     final ReserveRequestType reservationRequest = new NsiReservationFactory().setCorrelationId(
         UUID.randomUUID().toString()).createReservation();
-    final GenericAcknowledgmentType genericAcknowledgmentType = nsiProvider.reserve(reservationRequest);
-    assertEquals(reservationRequest.getCorrelationId(), genericAcknowledgmentType.getCorrelationId());
+    nsiProvider.reserve(reservationRequest);
   }
 
   @Test
   public void should_return_generic_acknowledgement_with_valid_correlation_id() throws Exception {
-    final ReserveRequestType reservationRequest = new NsiReservationFactory().createReservation();
+    final XMLGregorianCalendar startTime = DatatypeFactory.newInstance().newXMLGregorianCalendar();
+    startTime.setDay(10);
+    startTime.setMonth(10);
+    startTime.setYear(2012);
+    startTime.setMinute(0);
+    startTime.setHour(0);
+    startTime.setSecond(0);
+    
+    final XMLGregorianCalendar endTime = DatatypeFactory.newInstance().newXMLGregorianCalendar(
+        startTime.toGregorianCalendar());
+    endTime.setDay(startTime.getDay() + 5);
+
+    final ReserveRequestType reservationRequest = new NsiReservationFactory().setScheduleStartTime(startTime)
+        .setScheduleEndTime(endTime).createReservation();
     final GenericAcknowledgmentType genericAcknowledgmentType = nsiProvider.reserve(reservationRequest);
     assertEquals(reservationRequest.getCorrelationId(), genericAcknowledgmentType.getCorrelationId());
   }
