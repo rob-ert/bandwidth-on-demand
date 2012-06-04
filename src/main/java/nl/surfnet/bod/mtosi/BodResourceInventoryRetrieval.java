23b4c4d4f416b63535b6f2ce76a2ef7b47c443a0
robert <robert@localhost>
2012-06-04 20:11:54 +0200

    Removed comment

--- src/main/java/nl/surfnet/bod/mtosi/BodResourceInventoryRetrieval.java
+++ src/main/java/nl/surfnet/bod/mtosi/BodResourceInventoryRetrieval.java
@@ -9,11 +9,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.tmforum.mtop.mri.wsdl.rir.v1_0.ResourceInventoryRetrievalHttp;
 
 @WebServiceClient(name = "ResourceInventoryRetrievalHttp",
-    targetNamespace = "http://www.tmforum.org/mtop/mri/wsdl/rir/v1-0"
-/*
- * , wsdlLocation =
- * "file:/Users/robert/work/offline/clients/surfnet/bandwidth-on-demand/src/main/resources/MTOSI-2.1/DDPs/ManageResourceInventory/IIS/wsdl/ResourceInventoryRetrieval/ResourceInventoryRetrievalHttp.wsdl"
- */)
+    targetNamespace = "http://www.tmforum.org/mtop/mri/wsdl/rir/v1-0")
 public class BodResourceInventoryRetrieval extends ResourceInventoryRetrievalHttp {
 
   public BodResourceInventoryRetrieval() throws IOException {
