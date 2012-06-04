de3670de3181eaf86d6623156e474b01bcf96abc
Alan van Dam <avandam@zilverline.com>
2012-06-04 14:33:03 +0200

    make health check more healthy

--- src/main/java/nl/surfnet/bod/web/HealthCheckController.java
+++ src/main/java/nl/surfnet/bod/web/HealthCheckController.java
@@ -20,9 +20,34 @@ public class HealthCheckController {
   @RequestMapping(value = "/healthcheck")
   public String index(Model model) {
 
-    model.addAttribute("iddHealth", iddClient.getKlanten().size() > 0);
-    model.addAttribute("nbiHealth", nbiClient.getPhysicalPortsCount() > 0);
+    boolean iddHealth = isServiceHealty(new ServiceCheck() {
+      @Override
+      public boolean healty() {
+        return iddClient.getKlanten().size() > 0;
+      }});
+
+    boolean nbiHealth = isServiceHealty(new ServiceCheck() {
+      @Override
+      public boolean healty() {
+        return nbiClient.getPhysicalPortsCount() > 0;
+      }});
+
+    model.addAttribute("iddHealth", iddHealth);
+    model.addAttribute("nbiHealth", nbiHealth);
 
     return "healthcheck";
+  }
+
+  public boolean isServiceHealty(ServiceCheck check) {
+    try {
+      return check.healty();
+    }
+    catch (Exception e) {
+      return false;
+    }
+  }
+
+  interface ServiceCheck {
+    public boolean healty();
   }
 }
