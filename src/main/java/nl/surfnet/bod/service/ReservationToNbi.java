13e537a8e5918d8c6184b5b1cd6ef56ea6bb6047
robert <robert@localhost>
2012-06-26 14:59:20 +0200

    Removed async call to the NMS while creating a reservation.

--- src/main/java/nl/surfnet/bod/service/ReservationToNbi.java
+++ src/main/java/nl/surfnet/bod/service/ReservationToNbi.java
@@ -21,15 +21,14 @@
  */
 package nl.surfnet.bod.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nl.surfnet.bod.domain.Reservation;
 import nl.surfnet.bod.domain.ReservationStatus;
 import nl.surfnet.bod.nbi.NbiClient;
 import nl.surfnet.bod.repo.ReservationRepo;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
@@ -37,20 +36,18 @@ public class ReservationToNbi {
 
   @Autowired
   private NbiClient nbiClient;
+  
   @Autowired
   private ReservationRepo reservationRepo;
+  
   @Autowired
   private ReservationEventPublisher reservationEventPublisher;
 
-  @Async
   public void submitNewReservation(Long reservationId, boolean autoProvision) {
-    final Reservation reservation = reservationRepo.findOne(reservationId);
+    Reservation reservation = reservationRepo.findOne(reservationId);
     final ReservationStatus orgStatus = reservation.getStatus();
-
-    Reservation reservationWithReservationId = nbiClient.createReservation(reservation, autoProvision);
-
-    reservationRepo.save(reservationWithReservationId);
-
+    final Reservation reservationWithReservationId = nbiClient.createReservation(reservation, autoProvision);
+    reservation = reservationRepo.save(reservationWithReservationId);
     publishStatusChanged(reservationWithReservationId, orgStatus);
   }
 
@@ -58,7 +55,6 @@ public class ReservationToNbi {
     if (originalStatus == reservation.getStatus()) {
       return;
     }
-
     ReservationStatusChangeEvent createEvent = new ReservationStatusChangeEvent(originalStatus, reservation);
     reservationEventPublisher.notifyListeners(createEvent);
   }
