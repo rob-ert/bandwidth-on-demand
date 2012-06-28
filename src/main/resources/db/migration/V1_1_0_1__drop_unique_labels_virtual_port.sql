15ca922923b902a2c7eea48c94465592b1c8474b
Alan van Dam <avandam@zilverline.com>
2012-06-28 13:29:40 +0200

    fixing script if constraint is named different

--- src/main/resources/db/migration/V1_1_0_1__drop_unique_labels_virtual_port.sql
+++ src/main/resources/db/migration/V1_1_0_1__drop_unique_labels_virtual_port.sql
@@ -1,2 +1,3 @@
-ALTER TABLE virtual_port drop constraint virtual_port_manager_label_key;
+ALTER TABLE virtual_port drop constraint if exists virtual_port_manager_label_key;
+ALTER TABLE virtual_port drop constraint if exists virtual_port_name_key;
 ALTER TABLE virtual_port drop constraint virtual_port_user_label_key;
