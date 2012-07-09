51c60ae19d8025c526aa9f13da5719c2bddf0db2
robert <robert@localhost>
2012-07-09 11:40:39 +0200

    Removed drop's for non existing constraints. Made syntax psql 8 compatible.

--- src/main/resources/db/migration/V1_1_0_1__drop_unique_labels_virtual_port.sql
+++ src/main/resources/db/migration/V1_1_0_1__drop_unique_labels_virtual_port.sql
@@ -1,3 +1,1 @@
-ALTER TABLE virtual_port drop constraint if exists virtual_port_manager_label_key;
-ALTER TABLE virtual_port drop constraint if exists virtual_port_name_key;
-ALTER TABLE virtual_port drop constraint virtual_port_user_label_key;
+ALTER TABLE virtual_port drop constraint virtual_port_name_key;
