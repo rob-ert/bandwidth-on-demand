19f68a088d25ab901dac56295ae5c513f5b3f031
robert <robert@localhost>
2012-07-16 19:19:03 +0200

    Changed to Java 7 multi catch. Throwing IllegalArgumentException and not RuntimeException.

--- src/main/java/nl/surfnet/bod/util/ReflectiveFieldComparator.java
+++ src/main/java/nl/surfnet/bod/util/ReflectiveFieldComparator.java
@@ -1,5 +1,6 @@
 package nl.surfnet.bod.util;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Comparator;
 
 public class ReflectiveFieldComparator implements Comparator<Object> {
@@ -14,12 +15,12 @@ public class ReflectiveFieldComparator implements Comparator<Object> {
     try {
       if (one != null && two != null) {
         one = one.getClass().getMethod(getter, new Class[0]).invoke(one, new Object[0]);
-        two = two.getClass().getMethod(getter, new Class[0]).invoke(two, new Object[0]);
       }
     }
-    catch (Exception e) {
-      throw new RuntimeException(
-          "Cannot compare " + one + " with " + two + ". Does the getter: " + getter + " exists?", e);
+    catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException
+        | SecurityException e) {
+      throw new IllegalArgumentException("Cannot compare " + one + " with " + two + ". Does the getter: " + getter
+          + " exists?", e);
     }
     return (one == null) ? -1 : ((two == null) ? 1 : ((Comparable<Object>) one).compareTo(two));
   }
