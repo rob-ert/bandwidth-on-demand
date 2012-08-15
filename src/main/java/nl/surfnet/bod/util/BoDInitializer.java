6d4a867344f681c06dfde7dec26ee0c217c3fdb2
Frank Mölder <frank@wesaidso.com>
2012-08-15 10:36:25 +0200

    Using bonecp database
    Switch to this data pool due to issues with hibernate search and based
    on experience with load test results of an other project

--- src/main/java/nl/surfnet/bod/util/BoDInitializer.java
+++ src/main/java/nl/surfnet/bod/util/BoDInitializer.java
@@ -3,6 +3,9 @@ package nl.surfnet.bod.util;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.hibernate.search.MassIndexer;
+import org.hibernate.search.batchindexing.MassIndexerProgressMonitor;
+import org.hibernate.search.impl.SimpleIndexingProgressMonitor;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.Search;
 import org.slf4j.Logger;
@@ -21,14 +24,22 @@ public class BoDInitializer {
   public void init() {
     logger.info("Initializing BoD");
     indexDatabaseContent();
-    //IBM humor...
+    // IBM humor...
     logger.info("Ready for eBusiness");
   }
 
   public void indexDatabaseContent() {
     FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
+
+    MassIndexerProgressMonitor indexMonitor = new SimpleIndexingProgressMonitor(10);
     try {
-      fullTextEntityManager.createIndexer().startAndWait();
+      MassIndexer indexer = fullTextEntityManager.createIndexer();
+      //Set threads to one, to workaround bug of mass indexer 
+      //see (https://hibernate.onjira.com/browse/HSEARCH-598)
+      indexer.threadsForSubsequentFetching(1);
+      indexer.threadsToLoadObjects(1);
+      indexer.progressMonitor(indexMonitor);
+      indexer.startAndWait();
     }
     catch (InterruptedException e) {
       logger.error("Error indexing current database content", e);
