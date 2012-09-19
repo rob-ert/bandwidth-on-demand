5ae68fbb81cb96c1cabb7225343080be218f9480
Frank Mölder <frank@wesaidso.com>
2012-09-19 11:23:55 +0200

    Fixed unittest

--- src/test/java/nl/surfnet/bod/web/base/AbstractSearchableSortableListControllerTest.java
+++ src/test/java/nl/surfnet/bod/web/base/AbstractSearchableSortableListControllerTest.java
@@ -118,7 +118,7 @@ public class AbstractSearchableSortableListControllerTest {
   public void shouldMapTeamToVirtualResourceGroupName() {
     assertThat(subject.mapLabelToTechnicalName("blateam:bla"), is("blavirtualResourceGroup.name:bla"));
     assertThat(subject.mapLabelToTechnicalName("institute:test"), is("physicalResourceGroup.institute.name:test"));
-    assertThat(subject.mapLabelToTechnicalName("blaphysicalPort:test"), is("blaphysicalPort.id:test"));
+    assertThat(subject.mapLabelToTechnicalName("blaphysicalPort:test"), is("blaphysicalPort.nmsPortId:test"));
     assertThat(subject.mapLabelToTechnicalName("team:\"some-team\""), is("virtualResourceGroup.name:\"some-team\""));
   }
 
