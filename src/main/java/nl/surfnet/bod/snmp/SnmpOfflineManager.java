c61d332dc2f847584a7d577afa509301ceb6945d
robert <hellwig@gmail.com>
2012-09-24 17:03:29 +0200

    Changed to Spring component
    Properties are now retrieved from property files.

--- src/main/java/nl/surfnet/bod/snmp/SnmpOfflineManager.java
+++ src/main/java/nl/surfnet/bod/snmp/SnmpOfflineManager.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snmp4j.CommandResponder;
@@ -16,15 +18,16 @@ import org.snmp4j.Snmp;
 import org.snmp4j.mp.MPv2c;
 import org.snmp4j.security.SecurityProtocols;
 import org.snmp4j.smi.OctetString;
-import org.snmp4j.smi.TcpAddress;
 import org.snmp4j.smi.TransportIpAddress;
 import org.snmp4j.smi.UdpAddress;
 import org.snmp4j.transport.AbstractTransportMapping;
-import org.snmp4j.transport.DefaultTcpTransportMapping;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
 import org.snmp4j.util.MultiThreadedMessageDispatcher;
 import org.snmp4j.util.ThreadPool;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SnmpOfflineManager implements CommandResponder, Runnable {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
@@ -35,23 +38,25 @@ public class SnmpOfflineManager implements CommandResponder, Runnable {
 
   private final Thread commandResponder = new Thread(this);
 
-  public SnmpOfflineManager() {
-    try {
-      abstractTransportMapping = new DefaultUdpTransportMapping(new UdpAddress("localhost/1620"));
-    }
-    catch (IOException e) {
-      log.error("Error: ", e);
-    }
-  }
+  private TransportIpAddress transportIpAddress;
 
-  public SnmpOfflineManager(final TransportIpAddress transportIpAddress) {
+  @Value("${snmp.community}")
+  private String community;
+
+  @Value("${snmp.oid}")
+  private String oid;
+
+  @Value("${snmp.host}")
+  private String host;
+
+  @Value("${snmp.port}")
+  private String port;
+
+  @PostConstruct
+  private void init() {
+    transportIpAddress = new UdpAddress(host + port);
     try {
-      if (transportIpAddress instanceof TcpAddress) {
-        abstractTransportMapping = new DefaultTcpTransportMapping((TcpAddress) transportIpAddress);
-      }
-      else {
-        abstractTransportMapping = new DefaultUdpTransportMapping((UdpAddress) transportIpAddress);
-      }
+      abstractTransportMapping = new DefaultUdpTransportMapping((UdpAddress) transportIpAddress);
     }
     catch (IOException e) {
       log.error("Error: ", e);
@@ -64,17 +69,12 @@ public class SnmpOfflineManager implements CommandResponder, Runnable {
     final MessageDispatcher messageDispatcher = new MultiThreadedMessageDispatcher(ThreadPool.create("DispatcherPool",
         10), new MessageDispatcherImpl());
 
-    // add message processing models
-    // messageDispatcher.addMessageProcessingModel(new MPv1());
     messageDispatcher.addMessageProcessingModel(new MPv2c());
 
-    // add all security protocols
     SecurityProtocols.getInstance().addDefaultProtocols();
-    // SecurityProtocols.getInstance().addPrivacyProtocol(new Pri/v3DES());
 
-    // Create Target
     final CommunityTarget communityTarget = new CommunityTarget();
-    communityTarget.setCommunity(new OctetString("public"));
+    communityTarget.setCommunity(new OctetString(community));
 
     final Snmp snmp = new Snmp(messageDispatcher, abstractTransportMapping);
     snmp.addCommandResponder(this);
