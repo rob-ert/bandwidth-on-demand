52adf4c3f21ace69ef77e0eaaaee5a27393530c8
Frank Mölder <frank@wesaidso.com>
2012-09-25 18:15:27 +0200

    Added timezone test

--- src/test/integration/nl/surfnet/bod/nsi/ConnectionServiceProviderTestIntegration.java
+++ src/test/integration/nl/surfnet/bod/nsi/ConnectionServiceProviderTestIntegration.java
@@ -21,12 +21,6 @@
  */
 package nl.surfnet.bod.nsi;
 
-import static com.jayway.awaitility.Awaitility.await;
-import static nl.surfnet.bod.nsi.ws.ConnectionServiceProvider.URN_PROVIDER_NSA;
-import static nl.surfnet.bod.nsi.ws.ConnectionServiceProvider.URN_STP;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
@@ -35,23 +29,51 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
-import nl.surfnet.bod.domain.*;
+import nl.surfnet.bod.domain.Connection;
+import nl.surfnet.bod.domain.Institute;
+import nl.surfnet.bod.domain.PhysicalPort;
+import nl.surfnet.bod.domain.PhysicalResourceGroup;
+import nl.surfnet.bod.domain.Reservation;
+import nl.surfnet.bod.domain.ReservationStatus;
+import nl.surfnet.bod.domain.VirtualPort;
+import nl.surfnet.bod.domain.VirtualResourceGroup;
 import nl.surfnet.bod.nsi.ws.v1sc.ConnectionServiceProviderWs;
-import nl.surfnet.bod.repo.*;
+import nl.surfnet.bod.repo.ConnectionRepo;
+import nl.surfnet.bod.repo.InstituteRepo;
+import nl.surfnet.bod.repo.PhysicalPortRepo;
+import nl.surfnet.bod.repo.PhysicalResourceGroupRepo;
+import nl.surfnet.bod.repo.VirtualPortRepo;
+import nl.surfnet.bod.repo.VirtualResourceGroupRepo;
 import nl.surfnet.bod.service.ReservationEventPublisher;
 import nl.surfnet.bod.service.ReservationListener;
 import nl.surfnet.bod.service.ReservationService;
 import nl.surfnet.bod.service.ReservationStatusChangeEvent;
-import nl.surfnet.bod.support.*;
+import nl.surfnet.bod.support.ConnectionServiceProviderFactory;
+import nl.surfnet.bod.support.MockHttpServer;
+import nl.surfnet.bod.support.PhysicalPortFactory;
+import nl.surfnet.bod.support.PhysicalResourceGroupFactory;
+import nl.surfnet.bod.support.ReserveRequestTypeFactory;
+import nl.surfnet.bod.support.RichUserDetailsFactory;
+import nl.surfnet.bod.support.VirtualPortFactory;
+import nl.surfnet.bod.support.VirtualResourceGroupFactory;
+import nl.surfnet.bod.util.Environment;
 
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ogf.schemas.nsi._2011._10.connection._interface.*;
+import org.ogf.schemas.nsi._2011._10.connection._interface.GenericAcknowledgmentType;
+import org.ogf.schemas.nsi._2011._10.connection._interface.ProvisionRequestType;
+import org.ogf.schemas.nsi._2011._10.connection._interface.QueryRequestType;
+import org.ogf.schemas.nsi._2011._10.connection._interface.ReserveRequestType;
+import org.ogf.schemas.nsi._2011._10.connection._interface.TerminateRequestType;
 import org.ogf.schemas.nsi._2011._10.connection.provider.ServiceException;
 import org.ogf.schemas.nsi._2011._10.connection.types.ConnectionStateType;
 import org.ogf.schemas.nsi._2011._10.connection.types.PathType;
@@ -69,6 +91,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
+
+import static com.jayway.awaitility.Awaitility.await;
+
+import static nl.surfnet.bod.nsi.ws.ConnectionServiceProvider.URN_PROVIDER_NSA;
+import static nl.surfnet.bod.nsi.ws.ConnectionServiceProvider.URN_STP;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/appCtx.xml", "/spring/appCtx-jpa-integration.xml",
@@ -218,6 +248,35 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
     assertThat(connection.getCurrentState(), is(ConnectionStateType.TERMINATED));
   }
 
+  @Test
+  public void shouldReserveFromUTCTimeZone() throws Exception {
+    DateTime startUTC = new DateTime(DateTimeZone.UTC).plusHours(1);
+    DateTime startDefaultZone = new DateTime(startUTC).withZone(Environment.DEFAULT_TIME_ZONE);
+
+    ReserveRequestType reservationRequest = createReserveRequest(startUTC, startUTC.plusHours(1));
+    final String connectionId = reservationRequest.getReserve().getReservation().getConnectionId();
+    final String reserveCorrelationId = reservationRequest.getCorrelationId();
+
+    final DummyReservationListener listener = new DummyReservationListener();
+    reservationEventPublisher.addListener(listener);
+
+    GenericAcknowledgmentType reserveAcknowledgment = nsiProvider.reserve(reservationRequest);
+
+    assertThat(reserveAcknowledgment.getCorrelationId(), is(reserveCorrelationId));
+
+    final Connection connection = connectionRepo.findByConnectionId(connectionId);
+    assertThat(connection.getConnectionId(), is(connectionId));
+
+    listener.waitForEventWithNewStatus(ReservationStatus.RESERVED);
+
+    Reservation reservation = reservationService.find(connection.getReservation().getId());
+    entityManager.refresh(reservation);
+    entityManager.refresh(connection);
+
+    //Reservations are round the a whole minute
+    assertThat(reservation.getStartDateTime(), is(startDefaultZone.withSecondOfMinute(0).withMillisOfSecond(0)));
+  }
+
   private class DummyReservationListener implements ReservationListener {
     private Optional<ReservationStatusChangeEvent> event = Optional.absent();
 
@@ -238,8 +297,7 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
       await().atMost(10, TimeUnit.SECONDS).until(new Callable<Boolean>() {
         @Override
         public Boolean call() throws Exception {
-          return getEvent().isPresent()
-              && getEvent().get().getNewStatus().equals(status);
+          return getEvent().isPresent() && getEvent().get().getNewStatus().equals(status);
         }
       });
 
@@ -255,7 +313,6 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
     GenericAcknowledgmentType genericAcknowledgment = nsiProvider.query(queryRequest);
 
     assertThat(genericAcknowledgment.getCorrelationId(), is(queryRequest.getCorrelationId()));
-
 
   }
 
@@ -295,10 +352,35 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
     source.setStpId(URN_STP + ":" + destinationVirtualPort.getId());
     path.setSourceSTP(source);
 
-    ReserveRequestType reservationRequest = new ReserveRequestTypeFactory()
-       .setProviderNsa(URN_PROVIDER_NSA).setPath(path).create();
+    ReserveRequestType reservationRequest = new ReserveRequestTypeFactory().setProviderNsa(URN_PROVIDER_NSA)
+        .setPath(path).create();
 
     return reservationRequest;
+  }
+
+  private ReserveRequestType createReserveRequest(DateTime startDateTime, DateTime endDateTime)
+      throws DatatypeConfigurationException {
+    ReserveRequestType reserveRequest = createReserveRequest();
+
+    XMLGregorianCalendar start = toXMLGregorianCalander(startDateTime);
+    XMLGregorianCalendar end = toXMLGregorianCalander(endDateTime);
+
+    reserveRequest.getReserve().getReservation().getServiceParameters().getSchedule().setStartTime(start);
+    reserveRequest.getReserve().getReservation().getServiceParameters().getSchedule().setEndTime(end);
+
+    return reserveRequest;
+  }
+
+  private XMLGregorianCalendar toXMLGregorianCalander(DateTime timestamp) throws DatatypeConfigurationException {
+    int utcOffset = 0;
+
+    assertThat("Only UTC timestamps are supported", timestamp.getZone().getID(), is("UTC"));
+
+    XMLGregorianCalendar calendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(timestamp.getYear(),
+        timestamp.getMonthOfYear(), timestamp.getDayOfMonth(), timestamp.getHourOfDay(), timestamp.getMinuteOfHour(),
+        timestamp.getSecondOfMinute(), timestamp.getMillisOfSecond(), utcOffset);
+
+    return calendar;
   }
 
   private QueryRequestType createQueryRequest() {
