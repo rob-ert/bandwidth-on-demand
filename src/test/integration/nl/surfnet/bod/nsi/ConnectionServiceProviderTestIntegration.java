3984e12cc4ff257b3494370994bdbc85e050b3d1
Alan van Dam <avandam@zilverline.com>
2012-10-16 16:07:10 +0200

    fixing ConnectionServiceProviderTestIntegration

--- src/test/integration/nl/surfnet/bod/nsi/ConnectionServiceProviderTestIntegration.java
+++ src/test/integration/nl/surfnet/bod/nsi/ConnectionServiceProviderTestIntegration.java
@@ -21,6 +21,15 @@
  */
 package nl.surfnet.bod.nsi;
 
+import static com.jayway.awaitility.Awaitility.await;
+import static nl.surfnet.bod.nsi.ws.ConnectionServiceProvider.URN_PROVIDER_NSA;
+import static nl.surfnet.bod.nsi.ws.ConnectionServiceProvider.URN_STP;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertFalse;
+
+import java.util.EnumSet;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
@@ -30,34 +39,16 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.xml.datatype.DatatypeConfigurationException;
 
-import nl.surfnet.bod.domain.Connection;
-import nl.surfnet.bod.domain.Institute;
-import nl.surfnet.bod.domain.PhysicalPort;
-import nl.surfnet.bod.domain.PhysicalResourceGroup;
-import nl.surfnet.bod.domain.Reservation;
-import nl.surfnet.bod.domain.ReservationStatus;
-import nl.surfnet.bod.domain.VirtualPort;
-import nl.surfnet.bod.domain.VirtualResourceGroup;
+import nl.surfnet.bod.domain.*;
+import nl.surfnet.bod.domain.oauth.NsiScope;
 import nl.surfnet.bod.nsi.ws.v1sc.ConnectionServiceProviderFunctions;
 import nl.surfnet.bod.nsi.ws.v1sc.ConnectionServiceProviderWs;
-import nl.surfnet.bod.repo.ConnectionRepo;
-import nl.surfnet.bod.repo.InstituteRepo;
-import nl.surfnet.bod.repo.PhysicalPortRepo;
-import nl.surfnet.bod.repo.PhysicalResourceGroupRepo;
-import nl.surfnet.bod.repo.VirtualPortRepo;
-import nl.surfnet.bod.repo.VirtualResourceGroupRepo;
+import nl.surfnet.bod.repo.*;
 import nl.surfnet.bod.service.ReservationEventPublisher;
 import nl.surfnet.bod.service.ReservationListener;
 import nl.surfnet.bod.service.ReservationService;
 import nl.surfnet.bod.service.ReservationStatusChangeEvent;
-import nl.surfnet.bod.support.ConnectionServiceProviderFactory;
-import nl.surfnet.bod.support.MockHttpServer;
-import nl.surfnet.bod.support.PhysicalPortFactory;
-import nl.surfnet.bod.support.PhysicalResourceGroupFactory;
-import nl.surfnet.bod.support.ReserveRequestTypeFactory;
-import nl.surfnet.bod.support.RichUserDetailsFactory;
-import nl.surfnet.bod.support.VirtualPortFactory;
-import nl.surfnet.bod.support.VirtualResourceGroupFactory;
+import nl.surfnet.bod.support.*;
 import nl.surfnet.bod.web.security.RichUserDetails;
 import nl.surfnet.bod.web.security.Security;
 
@@ -70,11 +61,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ogf.schemas.nsi._2011._10.connection._interface.GenericAcknowledgmentType;
-import org.ogf.schemas.nsi._2011._10.connection._interface.ProvisionRequestType;
-import org.ogf.schemas.nsi._2011._10.connection._interface.QueryRequestType;
-import org.ogf.schemas.nsi._2011._10.connection._interface.ReserveRequestType;
-import org.ogf.schemas.nsi._2011._10.connection._interface.TerminateRequestType;
+import org.ogf.schemas.nsi._2011._10.connection._interface.*;
 import org.ogf.schemas.nsi._2011._10.connection.provider.ServiceException;
 import org.ogf.schemas.nsi._2011._10.connection.types.ConnectionStateType;
 import org.ogf.schemas.nsi._2011._10.connection.types.PathType;
@@ -93,16 +80,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
-
-import static com.jayway.awaitility.Awaitility.await;
-
-import static nl.surfnet.bod.nsi.ws.ConnectionServiceProvider.URN_PROVIDER_NSA;
-import static nl.surfnet.bod.nsi.ws.ConnectionServiceProvider.URN_STP;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertFalse;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/appCtx.xml", "/spring/appCtx-jpa-integration.xml",
@@ -150,7 +127,10 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
   private final String virtualResourceGroupName = "nsi:group";
   private VirtualPort sourceVirtualPort;
   private VirtualPort destinationVirtualPort;
-  private final RichUserDetails userDetails = new RichUserDetailsFactory().addUserGroup("test").create();
+  private final RichUserDetails userDetails = new RichUserDetailsFactory()
+    .setScopes(EnumSet.allOf(NsiScope.class))
+    .addBodRoles(BodRole.createUser())
+    .addUserGroup("test").create();
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
@@ -203,8 +183,6 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
 
   @Test
   public void shouldReserveProvisionTerminate() throws Exception {
-    Security.setUserDetails(new RichUserDetailsFactory().addUserGroup("test").addNocRole().create());
-
     ReserveRequestType reservationRequest = createReserveRequest();
     final String connectionId = reservationRequest.getReserve().getReservation().getConnectionId();
     final String reserveCorrelationId = reservationRequest.getCorrelationId();
@@ -260,7 +238,7 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
    * After reading from the database the timestamps are converted to the default
    * jvm timezone by the @See {@link PersistentDateTime} annotation on the
    * timestamp fields
-   * 
+   *
    * @throws Exception
    */
   @Test
@@ -306,7 +284,7 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
    * After reading from the database the timestamps are converted to the default
    * jvm timezone by the @See {@link PersistentDateTime} annotation on the
    * timestamp fields
-   * 
+   *
    * @throws Exception
    */
   @Test
@@ -405,7 +383,6 @@ public class ConnectionServiceProviderTestIntegration extends AbstractTransactio
   }
 
   private ReserveRequestType createReserveRequest() throws DatatypeConfigurationException {
-
     return createReserveRequest(Optional.<DateTime> absent(), Optional.<DateTime> absent());
   }
 
