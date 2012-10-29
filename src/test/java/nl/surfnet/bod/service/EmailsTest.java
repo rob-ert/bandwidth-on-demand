27291b0bc76a8eb38efddcff46e1270e9cd0c5fa
Alan van Dam <avandam@zilverline.com>
2012-10-29 13:43:40 +0100

    added another Emails test

--- src/test/java/nl/surfnet/bod/service/EmailsTest.java
+++ src/test/java/nl/surfnet/bod/service/EmailsTest.java
@@ -2,6 +2,11 @@ package nl.surfnet.bod.service;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpServletRequest;
+
 import nl.surfnet.bod.domain.VirtualPortRequestLink;
 import nl.surfnet.bod.support.RichUserDetailsFactory;
 import nl.surfnet.bod.support.VirtualPortRequestLinkFactory;
@@ -31,5 +36,34 @@ public class EmailsTest {
     String body = Emails.VirtualPortRequestMail.body(from, requestLink, link);
 
     assertThat(body, containsString("From: Henk (Unknown Email)"));
+  }
+
+
+  @Test
+  public void errorMailWithEmail() {
+    RichUserDetails user = new RichUserDetailsFactory().setDisplayname("Henk").setEmail("henk@henk.nl").create();
+    RuntimeException throwable = new RuntimeException();
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    when(request.getMethod()).thenReturn("GET");
+    when(request.getQueryString()).thenReturn("");
+    when(request.getRequestURL()).thenReturn(new StringBuffer("http://localhost/wrong"));
+
+    String body = Emails.ErrorMail.body(user, throwable, request);
+
+    assertThat(body, containsString("User: Henk (henk@henk.nl)"));
+  }
+
+  @Test
+  public void errorMailWithoutEmail() {
+    RichUserDetails user = new RichUserDetailsFactory().setDisplayname("Henk").setEmail(null).create();
+    RuntimeException throwable = new RuntimeException();
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    when(request.getMethod()).thenReturn("GET");
+    when(request.getQueryString()).thenReturn("");
+    when(request.getRequestURL()).thenReturn(new StringBuffer("http://localhost/wrong"));
+
+    String body = Emails.ErrorMail.body(user, throwable, request);
+
+    assertThat(body, containsString("User: Henk (Email not known)"));
   }
 }
