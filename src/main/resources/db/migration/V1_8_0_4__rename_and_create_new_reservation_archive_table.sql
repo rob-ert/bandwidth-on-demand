03541ebf6c8c38eaf8b33f6259f0360696b9a22e
robert <robert@localhost>
2012-11-20 13:24:37 +0100

    Renamed fk names.

--- src/main/resources/db/migration/V1_8_0_4__rename_and_create_new_reservation_archive_table.sql
+++ src/main/resources/db/migration/V1_8_0_4__rename_and_create_new_reservation_archive_table.sql
@@ -6,6 +6,6 @@ CREATE TABLE reservation_archive
   reservation_as_json text NOT NULL,
   reservation_primary_key bigint NOT NULL,
   version integer,
-  CONSTRAINT reservation_archive_pkey PRIMARY KEY (id),
-  CONSTRAINT reservation_archive_reservation_id_key UNIQUE (reservation_primary_key)
+  CONSTRAINT reservation_archive_new_pkey PRIMARY KEY (id),
+  CONSTRAINT reservation_archive_new_reservation_id_key UNIQUE (reservation_primary_key)
 );
