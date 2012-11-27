e1015db1c9b11a256fcb87657869b1153035c087
Frank Mölder <frank@wesaidso.com>
2012-11-27 12:28:19 +0100

    Created abstract controller with tests

--- src/main/java/nl/surfnet/bod/web/base/AbstractReportController.java
+++ src/main/java/nl/surfnet/bod/web/base/AbstractReportController.java
@@ -19,7 +19,7 @@
  * If the BSD license cannot be found with this distribution, it is available
  * at the following location <http://www.opensource.org/licenses/BSD-3-Clause>
  */
-package nl.surfnet.bod.web.noc;
+package nl.surfnet.bod.web.base;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,14 +31,14 @@ import nl.surfnet.bod.domain.ReservationStatus;
 import nl.surfnet.bod.event.LogEvent;
 import nl.surfnet.bod.service.LogEventService;
 import nl.surfnet.bod.service.ReservationService;
-import nl.surfnet.bod.util.Environment;
-import nl.surfnet.bod.web.security.RichUserDetails;
-import nl.surfnet.bod.web.security.Security;
 import nl.surfnet.bod.web.view.NocReservationReport;
+import nl.surfnet.bod.web.view.ReportIntervalView;
 
 import org.joda.time.DateMidnight;
 import org.joda.time.DateTime;
-import org.springframework.stereotype.Controller;
+import org.joda.time.Interval;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -50,13 +50,8 @@ import static nl.surfnet.bod.domain.ReservationStatus.SCHEDULED;
 import static nl.surfnet.bod.domain.ReservationStatus.TRANSITION_STATES;
 import static nl.surfnet.bod.domain.ReservationStatus.TRANSITION_STATES_AS_ARRAY;
 
-@Controller("nocReportController")
-@RequestMapping(ReportController.PAGE_URL)
-public class ReportController {
-  public static final String PAGE_URL = "noc/report";
-
-  @Resource(name = "bodEnvironment")
-  private Environment environment;
+public abstract class AbstractReportController {
+  protected static final int AMOUNT_OF_REPORT_PERIODS = 8;
 
   @Resource
   private ReservationService reservationService;
@@ -67,26 +62,62 @@ public class ReportController {
   @RequestMapping(method = RequestMethod.GET)
   public String index(Model model) {
 
-    model.addAttribute("report", determineReport(Security.getUserDetails()));
+    model.addAttribute("report", determineReport());
 
-    return PAGE_URL;
+    return getPageUrl();
   }
 
-  private NocReservationReport determineReport(RichUserDetails userDetails) {
+  protected abstract String getPageUrl();
+
+  protected abstract List<String> getAdminGroups();
+
+  @VisibleForTesting
+  List<ReportIntervalView> determineReportIntervals() {
+    final DateTimeFormatter labelFormatter = DateTimeFormat.forPattern("yyyy MMM");
+    final List<ReportIntervalView> reportIntervals = new ArrayList<>();
+
+    String label;
+    Interval reportInterval;
+    DateTime firstDayOfInterval;
+    DateTime lastDayOfInterval;
+
+    for (int i = 0; i < AMOUNT_OF_REPORT_PERIODS; i++) {
+      firstDayOfInterval = DateMidnight.now().withDayOfMonth(1).minusMonths(i).toDateTime();
+      lastDayOfInterval = firstDayOfInterval.dayOfMonth().withMaximumValue().toDateTime();
+
+      // Correct to today
+      if (lastDayOfInterval.isAfterNow()) {
+        lastDayOfInterval = firstDayOfInterval.withDayOfMonth(DateTime.now().getDayOfMonth());
+        label = "Current month until now";
+      }
+      else {
+        label = labelFormatter.print(firstDayOfInterval);
+      }
+
+      reportInterval = new Interval(firstDayOfInterval, lastDayOfInterval);
+      reportIntervals.add(new ReportIntervalView(reportInterval, label));
+    }
+
+    return reportIntervals;
+  }
+
+  private NocReservationReport determineReport() {
     // TODO previous month
     final DateTime firstDayOfPreviousMonth = DateMidnight.now().withDayOfMonth(1).toDateTime();
     final DateTime lastDayOfPreviousMonth = firstDayOfPreviousMonth.dayOfMonth().withMaximumValue().toDateTime();
 
+    List<String> adminGroups = getAdminGroups();
+
     NocReservationReport nocReservationReport = new NocReservationReport(firstDayOfPreviousMonth,
         lastDayOfPreviousMonth);
-    determineReservationRequests(nocReservationReport);
-    determineReservationsForProtectionType(nocReservationReport);
-    determineActiveRunningReservations(nocReservationReport);
+    determineReservationRequestsForGroups(nocReservationReport, adminGroups);
+    determineReservationsForProtectionType(nocReservationReport, adminGroups);
+    determineActiveRunningReservations(nocReservationReport, adminGroups);
 
     return nocReservationReport;
   }
 
-  private void determineReservationRequests(NocReservationReport nocReservationReport) {
+  private void determineReservationRequestsForGroups(NocReservationReport nocReservationReport, List<String> adminGroups) {
     final DateTime start = nocReservationReport.getPeriodStart();
     final DateTime end = nocReservationReport.getPeriodEnd();
 
@@ -114,7 +145,7 @@ public class ReportController {
   }
 
   @VisibleForTesting
-  void determineReservationsForProtectionType(NocReservationReport nocReservationReport) {
+  void determineReservationsForProtectionType(NocReservationReport nocReservationReport, List<String> adminGroups) {
     final DateTime start = nocReservationReport.getPeriodStart();
     final DateTime end = nocReservationReport.getPeriodEnd();
 
@@ -140,7 +171,7 @@ public class ReportController {
   }
 
   @VisibleForTesting
-  void determineActiveRunningReservations(NocReservationReport nocReservationReport) {
+  void determineActiveRunningReservations(NocReservationReport nocReservationReport, List<String> adminGroups) {
     final DateTime start = nocReservationReport.getPeriodStart();
     final DateTime end = nocReservationReport.getPeriodEnd();
 
---
+++ src/main/java/nl/surfnet/bod/web/view/ReportIntervalView.java
@@ -0,0 +1,32 @@
+package nl.surfnet.bod.web.view;
+
+import org.joda.time.Interval;
+
+public class ReportIntervalView {
+
+  private final int id;
+  private final Interval interval;
+  private final String label;
+
+  public ReportIntervalView(Interval interval, String label) {
+    super();
+    this.interval = interval;
+    this.label = label;
+
+    // Id will be 201210 when start is oct 2012
+    this.id = interval.getStart().getMonthOfYear() + (interval.getStart().getMonthOfYear() * 100);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public Interval getInterval() {
+    return interval;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+}
