94f089eb7e17de94095289d99ce1cf2f3a584daf
Alan van Dam <avandam@zilverline.com>
2012-12-19 15:57:15 +0100

    encrypt idd password, small improvemnt scala encrypt util

--- src/test/scala/nl/surfnet/bod/EncryptedString.scala
+++ src/test/scala/nl/surfnet/bod/EncryptedString.scala
@@ -1,19 +1,23 @@
 package nl.surfnet.bod;
 
 import org.jasypt.intf.service.JasyptStatelessService
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor
 
 object EncryptedString {
 
+  val encryptor = {
+    val password = sys.env.getOrElse("BOD_ENCRYPTION_PASSWORD", sys.error("Encryption password is not set"))
+
+    val enc = new StandardPBEStringEncryptor()
+    enc.setAlgorithm("PBEWithMD5AndTripleDES")
+    enc.setPassword(password)
+    enc
+  }
+
   implicit def stringToEncryptedString(str: String) = new {
 
-    val service = new JasyptStatelessService();
-    val password = sys.env.getOrElse("BOD_ENCRYPTION_PASSWORD", sys.error("Encryption password is not set"))
-    val algo = "PBEWithMD5AndTripleDES";
+    def encrypt(): String = encryptor.encrypt(str)
 
-    def encrypt(): String = service.encrypt(str, password, null, null, algo, null, null, null, null, null, null,
-        null, null, null, null, null, null, null, null, null, null, null);
-
-    def decrypt(): String = service.decrypt(str, password, null, null, algo, null, null, null, null, null, null,
-        null, null, null, null, null, null, null, null, null, null, null);
+    def decrypt(): String = encryptor.decrypt(str)
   }
 }
