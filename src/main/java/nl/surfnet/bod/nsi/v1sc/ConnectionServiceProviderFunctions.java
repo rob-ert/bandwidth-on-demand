f9b25db2b075471468781f20b121078949268dd3
Alan van Dam <avandam@zilverline.com>
2013-02-07 14:46:00 +0100

    removed wsdls from main/resources that are bundeled in the separate jar files

--- src/main/java/nl/surfnet/bod/nsi/v1sc/ConnectionServiceProviderFunctions.java
+++ src/main/java/nl/surfnet/bod/nsi/v1sc/ConnectionServiceProviderFunctions.java
@@ -60,54 +60,61 @@ import com.google.common.base.Optional;
 
 public final class ConnectionServiceProviderFunctions {
 
-  public static final Function<NsiRequestDetails, ConnectionRequesterPort> NSI_REQUEST_TO_CONNECTION_REQUESTER_PORT = //
-  new Function<NsiRequestDetails, ConnectionRequesterPort>() {
-    @Override
-    public ConnectionRequesterPort apply(final NsiRequestDetails requestDetails) {
-      URL url;
-      try {
-        url = new ClassPathResource("/wsdl/nsi/ogf_nsi_connection_requester_v1_0.wsdl").getURL();
+  private static final QName SERVICE_NAME =
+    new QName("http://schemas.ogf.org/nsi/2011/10/connection/requester", "ConnectionServiceRequester");
+
+  private static final String WSDL_LOCATION = "/wsdl/ogf_nsi_connection_requester_v1_0.wsdl";
+
+  public static final Function<NsiRequestDetails, ConnectionRequesterPort> NSI_REQUEST_TO_CONNECTION_REQUESTER_PORT =
+    new Function<NsiRequestDetails, ConnectionRequesterPort>() {
+      @Override
+      public ConnectionRequesterPort apply(final NsiRequestDetails requestDetails) {
+          ConnectionRequesterPort port = new ConnectionServiceRequester(getWsdlUrl(), SERVICE_NAME)
+            .getConnectionServiceRequesterPort();
+
+          Map<String, Object> requestContext = ((BindingProvider) port).getRequestContext();
+          requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, requestDetails.getReplyTo());
+
+          return port;
       }
-      catch (IOException e) {
-        throw new RuntimeException("Could not find the requester wsdl", e);
+
+      private URL getWsdlUrl() {
+        try {
+          URL url = new ClassPathResource(WSDL_LOCATION).getURL();
+          return url;
+        }
+        catch (IOException e) {
+          throw new RuntimeException("Could not find the requester wsdl", e);
+        }
       }
-      final ConnectionRequesterPort port = new ConnectionServiceRequester(url, new QName(
-          "http://schemas.ogf.org/nsi/2011/10/connection/requester", "ConnectionServiceRequester"))
-          .getConnectionServiceRequesterPort();
+    };
 
-      final Map<String, Object> requestContext = ((BindingProvider) port).getRequestContext();
-      requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, requestDetails.getReplyTo());
-      return port;
-    }
-  };
+  public static final Function<Connection, GenericFailedType> CONNECTION_TO_GENERIC_FAILED =
+    new Function<Connection, GenericFailedType>() {
+      @Override
+      public GenericFailedType apply(final Connection connection) {
+        final GenericFailedType generic = new GenericFailedType();
+        generic.setProviderNSA(connection.getProviderNsa());
+        generic.setRequesterNSA(connection.getRequesterNsa());
+        generic.setConnectionId(connection.getConnectionId());
+        generic.setGlobalReservationId(connection.getGlobalReservationId());
+        generic.setConnectionState(connection.getCurrentState());
+        return generic;
+      }
+    };
 
-  public static final Function<Connection, GenericFailedType> CONNECTION_TO_GENERIC_FAILED = //
-  new Function<Connection, GenericFailedType>() {
-    @Override
-    public GenericFailedType apply(final Connection connection) {
-      final GenericFailedType generic = new GenericFailedType();
-      generic.setProviderNSA(connection.getProviderNsa());
-      generic.setRequesterNSA(connection.getRequesterNsa());
-      generic.setConnectionId(connection.getConnectionId());
-      generic.setGlobalReservationId(connection.getGlobalReservationId());
-      generic.setConnectionState(connection.getCurrentState());
-      return generic;
-    }
-  };
-
-
-  public static final Function<Connection, GenericConfirmedType> CONNECTION_TO_GENERIC_CONFIRMED = //
-  new Function<Connection, GenericConfirmedType>() {
-    @Override
-    public GenericConfirmedType apply(final Connection connection) {
-      final GenericConfirmedType generic = new GenericConfirmedType();
-      generic.setProviderNSA(connection.getProviderNsa());
-      generic.setRequesterNSA(connection.getRequesterNsa());
-      generic.setConnectionId(connection.getConnectionId());
-      generic.setGlobalReservationId(connection.getGlobalReservationId());
-      return generic;
-    }
-  };
+  public static final Function<Connection, GenericConfirmedType> CONNECTION_TO_GENERIC_CONFIRMED =
+    new Function<Connection, GenericConfirmedType>() {
+      @Override
+      public GenericConfirmedType apply(final Connection connection) {
+        final GenericConfirmedType generic = new GenericConfirmedType();
+        generic.setProviderNSA(connection.getProviderNsa());
+        generic.setRequesterNSA(connection.getRequesterNsa());
+        generic.setConnectionId(connection.getConnectionId());
+        generic.setGlobalReservationId(connection.getGlobalReservationId());
+        return generic;
+      }
+    };
 
   public static final Function<ReserveRequestType, Connection> RESERVE_REQUEST_TO_CONNECTION =
     new Function<ReserveRequestType, Connection>() {
