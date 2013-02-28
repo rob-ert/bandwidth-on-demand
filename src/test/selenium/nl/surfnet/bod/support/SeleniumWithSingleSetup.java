d4a471da36e271350d2afc994bb3096ca0008ebd
Alan van Dam <avandam@zilverline.com>
2013-02-28 17:47:13 +0100

    fix running multiple selenium tests that extend SeleniumWithSingleSetup

--- src/test/selenium/nl/surfnet/bod/support/SeleniumWithSingleSetup.java
+++ src/test/selenium/nl/surfnet/bod/support/SeleniumWithSingleSetup.java
@@ -3,10 +3,16 @@ package nl.surfnet.bod.support;
 import nl.surfnet.bod.service.DatabaseTestHelper;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 public abstract class SeleniumWithSingleSetup extends TestExternalSupport {
 
   private static boolean setupDone;
+
+  @BeforeClass
+  public static void clearSetupDone() {
+    setupDone = false;
+  }
 
   @Before
   public void setup() {
