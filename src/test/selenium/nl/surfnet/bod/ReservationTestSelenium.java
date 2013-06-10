1220ed49c8316cdf2c99a1d12430a60d7b4c9aff
Hans Westerbeek <hanswesterbeek@gmail.com>
2013-06-10 17:51:57 +0200

    WIP on functional tests for NSI v2 SOAP api

--- src/test/selenium/nl/surfnet/bod/ReservationTestSelenium.java
+++ src/test/selenium/nl/surfnet/bod/ReservationTestSelenium.java
@@ -22,16 +22,59 @@
  */
 package nl.surfnet.bod;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Holder;
+import javax.xml.ws.handler.MessageContext;
+
+import nl.surfnet.bod.nsi.NsiHelper;
 import nl.surfnet.bod.service.DatabaseTestHelper;
 import nl.surfnet.bod.support.ReservationFilterViewFactory;
 import nl.surfnet.bod.support.SeleniumWithSingleSetup;
 
+import nl.surfnet.bod.support.soap.SoapReplyListener;
 import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
 import org.joda.time.LocalTime;
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.ogf.schemas.nsi._2013._04.connection.provider.ConnectionProviderPort;
+import org.ogf.schemas.nsi._2013._04.connection.provider.ConnectionServiceProvider;
+import org.ogf.schemas.nsi._2013._04.connection.types.DirectionalityType;
+import org.ogf.schemas.nsi._2013._04.connection.types.PathType;
+import org.ogf.schemas.nsi._2013._04.connection.types.ReservationRequestCriteriaType;
+import org.ogf.schemas.nsi._2013._04.connection.types.ScheduleType;
+import org.ogf.schemas.nsi._2013._04.connection.types.StpType;
+import org.ogf.schemas.nsi._2013._04.framework.headers.CommonHeaderType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 
 public class ReservationTestSelenium extends SeleniumWithSingleSetup {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ReservationTestSelenium.class);
+
+  private static final String WSDL_LOCATION = "/wsdl/2.0/ogf_nsi_connection_provider_v2_0.wsdl";
+  private static final String ENDPOINT = "http://localhost:8083/bod/nsi/v2/provider";
+
+  private static final Integer REPLY_SERVER_PORT = 31337;
+  private static final String REPLY_PATH = "http://localhost:" + REPLY_SERVER_PORT + "/";
+
+  private String OAUTH_TOKEN = "a7dc882f-64b2-40d6-a208-3f59efb7a7ef";
+
+  private SoapReplyListener soapReplyListener;
+  private ConnectionProviderPort connectionServiceProviderPort;
 
   @Override
   public void setupInitialData() {
@@ -50,6 +93,17 @@ public class ReservationTestSelenium extends SeleniumWithSingleSetup {
     getUserDriver().selectInstituteAndRequest(GROUP_SURFNET, 1200, "port 2");
     getWebDriver().clickLinkInLastEmail();
     getManagerDriver().createVirtualPort("Second port");
+  }
+
+  @Before
+  public void before(){
+
+    // set and retrieve the oauth token that we can use when using the soap api
+
+
+    soapReplyListener = new SoapReplyListener(REPLY_SERVER_PORT);
+    connectionServiceProviderPort = createPort();
+
   }
 
   @After
@@ -119,6 +173,81 @@ public class ReservationTestSelenium extends SeleniumWithSingleSetup {
 
     // Search on odd
     getUserDriver().verifyReservationByFilterAndSearch(ReservationFilterViewFactory.COMING, "odd", odd);
+
   }
 
+  @Test
+  @Ignore
+  public void createAndCancelAReservationThroughSoap() {
+
+    LocalDateTime startTime = LocalDateTime.now().plusHours(1);
+    LocalDateTime endTime = LocalDateTime.now();
+
+
+    try {
+
+      GregorianCalendar gregorianCalendar = new GregorianCalendar();
+
+      gregorianCalendar.setTime(startTime.toDate());
+      XMLGregorianCalendar xmlGregorianCalendarStart = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+
+      gregorianCalendar.setTime(endTime.toDate());
+      XMLGregorianCalendar xmlGregorianCalendarEnd = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+
+
+      String description = "NSI v2 Reservation";
+      String globalReservationId = NsiHelper.generateGlobalReservationId();
+      ReservationRequestCriteriaType criteria = new ReservationRequestCriteriaType();
+
+      criteria.setSchedule(new ScheduleType().withStartTime(xmlGregorianCalendarStart).withEndTime(xmlGregorianCalendarEnd));
+      criteria.setBandwidth(100);
+      criteria.setPath(new PathType()
+          .withDirectionality(DirectionalityType.BIDIRECTIONAL)
+          .withSourceSTP(new StpType().withNetworkId(NMS_PORT_ID_1))
+          .withDestSTP(new StpType().withNetworkId(NMS_PORT_ID_2))
+      );
+
+      connectionServiceProviderPort.reserve(null, globalReservationId, description, criteria, getHeader());
+      String reply = soapReplyListener.waitForReply();
+      LOG.debug(reply);
+      assertTrue(reply != null);
+      // assert that it was indeed created
+
+      // cancel it (as a NOC manager?)
+
+      // verify that it is indeed cancelled.
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Holder<CommonHeaderType> getHeader(){
+    return new Holder<CommonHeaderType>(new CommonHeaderType()
+        .withProtocolVersion("2.0")
+        .withRequesterNSA("reqNSA")
+        .withProviderNSA("provnsa")
+        .withReplyTo(REPLY_PATH)
+    );
+  }
+
+  private ConnectionProviderPort createPort() {
+
+
+    ConnectionProviderPort port = new ConnectionServiceProvider(wsdlUrl()).getConnectionServiceProviderPort();
+    ((BindingProvider) port).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, ENDPOINT);
+
+    ((BindingProvider) port).getRequestContext().put(MessageContext.HTTP_REQUEST_HEADERS, Collections.singletonMap("Authorization", Collections.singletonList("bearer " + OAUTH_TOKEN)));
+
+    return port;
+  }
+
+
+  private URL wsdlUrl() {
+    try {
+      return new ClassPathResource(WSDL_LOCATION).getURL();
+    }
+    catch (IOException e) {
+      throw new RuntimeException("Could not find the requester wsdl", e);
+    }
+  }
 }
---
+++ src/test/selenium/nl/surfnet/bod/support/soap/SoapReplyListener.java
@@ -0,0 +1,225 @@
+/**
+ * Copyright (c) 2012, SURFnet BV
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ *     disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the SURFnet BV nor the names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package nl.surfnet.bod.support.soap;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Stack;
+
+import org.apache.http.ConnectionClosedException;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
+import org.apache.http.HttpServerConnection;
+import org.apache.http.impl.DefaultConnectionReuseStrategy;
+import org.apache.http.impl.DefaultHttpResponseFactory;
+import org.apache.http.impl.DefaultHttpServerConnection;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.SyncBasicHttpParams;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpProcessor;
+import org.apache.http.protocol.HttpRequestHandler;
+import org.apache.http.protocol.HttpRequestHandlerRegistry;
+import org.apache.http.protocol.HttpService;
+import org.apache.http.protocol.ImmutableHttpProcessor;
+import org.apache.http.protocol.ResponseConnControl;
+import org.apache.http.protocol.ResponseContent;
+import org.apache.http.protocol.ResponseDate;
+import org.apache.http.protocol.ResponseServer;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple server that always replies "200 OK" and exposes a stack of response objects,
+ * which you obviously should only use pop() on.
+ *
+ */
+public class SoapReplyListener {
+
+  private static final Logger LOG = LoggerFactory.getLogger(SoapReplyListener.class);
+
+  public Stack<String> getResponses() {
+    return responses;
+  }
+
+  private Stack<String> responses = new Stack<>();
+
+  public String waitForReply(){
+
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      throw new IllegalStateException("No reply received in time!");
+    }
+
+    return responses.pop();
+  }
+
+
+  public SoapReplyListener(Integer port) {
+
+    try {
+      Thread t = new RequestListenerThread(port, responses);
+      t.setDaemon(false);
+      t.start();
+    } catch (IOException e) {
+      LOG.error("Unable to start soap reply listener... Something else still running on our port? (=" + port + ") ", e);
+    }
+
+  }
+
+  static class SoapPostRequestHandler implements HttpRequestHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SoapPostRequestHandler.class);
+
+    private Stack<String> responses;
+
+    SoapPostRequestHandler(Stack<String> responses) {
+      this.responses = responses;
+    }
+
+    @Override
+    public void handle(HttpRequest request, HttpResponse response, HttpContext context) throws HttpException, IOException {
+      LOG.debug("Handling request: " + request);
+
+      if (! (request instanceof  HttpEntityEnclosingRequest)){
+        throw new IllegalArgumentException("Request did not have a body: " + request);
+      }
+
+      HttpEntityEnclosingRequest httpEntityEnclosingRequest = (HttpEntityEnclosingRequest) request;
+      responses.push(EntityUtils.toString(httpEntityEnclosingRequest.getEntity(), "UTF-8"));
+      response.setStatusCode(200);
+    }
+  }
+
+  static class RequestListenerThread extends Thread {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RequestListenerThread.class);
+
+    private final ServerSocket serversocket;
+    private final HttpParams params;
+    private final HttpService httpService;
+
+    public RequestListenerThread(int port, Stack<String> responses) throws IOException {
+      this.serversocket = new ServerSocket(port);
+      this.params = new SyncBasicHttpParams();
+      this.params
+          .setIntParameter(CoreConnectionPNames.SO_TIMEOUT, 5000)
+          .setIntParameter(CoreConnectionPNames.SOCKET_BUFFER_SIZE, 8 * 1024)
+          .setBooleanParameter(CoreConnectionPNames.STALE_CONNECTION_CHECK, false)
+          .setBooleanParameter(CoreConnectionPNames.TCP_NODELAY, true)
+          .setParameter(CoreProtocolPNames.ORIGIN_SERVER, "HttpComponents/1.1");
+
+      // Set up the HTTP protocol processor
+      HttpProcessor httpProcessor = new ImmutableHttpProcessor(new HttpResponseInterceptor[] {
+          new ResponseDate(),
+          new ResponseServer(),
+          new ResponseContent(),
+          new ResponseConnControl()
+      });
+
+      // Set up request handlers
+      HttpRequestHandlerRegistry reqistry = new HttpRequestHandlerRegistry();
+      reqistry.register("*", new SoapPostRequestHandler(responses));
+
+      // Set up the HTTP service
+      this.httpService = new HttpService(
+          httpProcessor,
+          new DefaultConnectionReuseStrategy(),
+          new DefaultHttpResponseFactory(),
+          reqistry,
+          this.params);
+    }
+
+    @Override
+    public void run() {
+      LOG.debug("Listening on port " + this.serversocket.getLocalPort());
+      while (!Thread.interrupted()) {
+        try {
+          // Set up HTTP connection
+          Socket socket = this.serversocket.accept();
+          DefaultHttpServerConnection conn = new DefaultHttpServerConnection();
+          LOG.debug("Incoming connection from " + socket.getInetAddress());
+          conn.bind(socket, this.params);
+
+          // Start worker thread
+          Thread t = new WorkerThread(this.httpService, conn);
+          t.setDaemon(true);
+          t.start();
+        } catch (InterruptedIOException ex) {
+          LOG.debug("interrupted!", ex);
+          break;
+        } catch (IOException e) {
+          LOG.error("I/O error initialising connection thread: " + e.getMessage(), e);
+          break;
+        }
+      }
+    }
+  }
+
+  static class WorkerThread extends Thread {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RequestListenerThread.class);
+
+    private final HttpService httpservice;
+    private final HttpServerConnection conn;
+
+    public WorkerThread(
+        final HttpService httpservice,
+        final HttpServerConnection conn) {
+      super();
+      this.httpservice = httpservice;
+      this.conn = conn;
+    }
+
+    @Override
+    public void run() {
+      LOG.debug("New connection thread");
+      HttpContext context = new BasicHttpContext(null);
+      try {
+        while (!Thread.interrupted() && this.conn.isOpen()) {
+          this.httpservice.handleRequest(this.conn, context);
+        }
+      } catch (ConnectionClosedException ex) {
+        LOG.error("Client closed connection");
+      } catch (IOException ex) {
+        LOG.error("I/O error: " + ex.getMessage());
+      } catch (HttpException ex) {
+        LOG.error("Unrecoverable HTTP protocol violation: " + ex.getMessage());
+      } finally {
+        try {
+          this.conn.shutdown();
+        } catch (IOException ignore) {}
+      }
+    }
+
+  }
+}
