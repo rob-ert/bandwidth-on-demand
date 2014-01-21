93770b7c2b10a6fff25003ab692ecad9dad3c8eb
Hans Westerbeek <hanswesterbeek@gmail.com>
2014-01-21 10:39:18 +0100

    Add index on nsi_v2_message.connection_id as the table is likely to grow large

--- src/main/resources/db/migration/V1_22_0_0__queryresult.sql
+++ src/main/resources/db/migration/V1_22_0_0__queryresult.sql
@@ -1,2 +1,4 @@
 ALTER TABLE nsi_v2_message ADD COLUMN result_id bigint;
 ALTER TABLE nsi_v2_message ADD COLUMN connection_id varchar(255);
+
+CREATE INDEX nsi_v2_message_connection_id_idx ON nsi_v2_message (connection_id);
