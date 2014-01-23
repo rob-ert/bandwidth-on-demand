0c7424324a1685cfbb71af671d340f00b4e28239
Hans Westerbeek <hanswesterbeek@gmail.com>
2014-01-23 23:04:02 +0100

    Naive switching seems to work: sending a unsubscribe to the new server causes the old server to stop sending us messages

--- src/main/java/nl/surfnet/bod/nbi/onecontrol/BackendSwitchListenerHandler.java
+++ src/main/java/nl/surfnet/bod/nbi/onecontrol/BackendSwitchListenerHandler.java
@@ -26,6 +26,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import javax.annotation.Resource;
 import javax.xml.namespace.QName;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
@@ -36,12 +38,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+/**
+ * Listens for switches that occur behind the haproxy that hides the onecontrol backend
+ * When a switch occurs, we must attempt to unsubscribe from our previous server, and re-subscribe to
+ * the new one so that we receive notifications from the same server as we are sending inventory/reservation requests to
+ */
 @Component
 @Profile("onecontrol")
 public class BackendSwitchListenerHandler implements SOAPHandler<SOAPMessageContext> {
 
   private static final Logger LOG = LoggerFactory.getLogger(BackendSwitchListenerHandler.class);
   public static final String BACKEND_SERVER_ID_HEADER = "X-Backend-Server";
+
+  private AtomicReference<String> lastKnownServer = new AtomicReference<>();
+
+  @Resource
+  private NotificationSubscriber notificationSubscriber;
 
   @Override
   public Set<QName> getHeaders() {
@@ -53,14 +65,26 @@ public class BackendSwitchListenerHandler implements SOAPHandler<SOAPMessageCont
 
     boolean outbound = (boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
 
-    if (!outbound){ // only look at responses to queries made by our clients
+    if (!outbound) { // only look at responses to queries made by our clients
 
       @SuppressWarnings("unchecked") // the documentation states that we are returned this type
       Map<String, List<String>> headers = (Map<String, List<String>>) context.get(MessageContext.HTTP_RESPONSE_HEADERS);
 
       if (headers.containsKey(BACKEND_SERVER_ID_HEADER)){
         final List<String> backendServerValues = headers.get(BACKEND_SERVER_ID_HEADER);
-        LOG.debug("Received reply from backend server: {}", backendServerValues.get(0));
+        final String currentBackendServer = backendServerValues.get(0);
+        if (lastKnownServer.get() == null) {
+          lastKnownServer.set(currentBackendServer); // it is the first request
+        } else if (!lastKnownServer.get().equals(currentBackendServer)){
+          // a switch has occured
+          LOG.info("Switch detected, switching notification subscription from {} to {}", lastKnownServer.get(), currentBackendServer);
+          synchronized (notificationSubscriber){
+            notificationSubscriber.unsubscribe(); // will it just work if we send our unsubscription to the new server?
+            notificationSubscriber.subscribe(); // this will go to the new backendserver automatically
+            lastKnownServer.set(currentBackendServer);
+          }
+        }
+        LOG.debug("Received reply from backend server: {}", currentBackendServer);
       }
 
     }
