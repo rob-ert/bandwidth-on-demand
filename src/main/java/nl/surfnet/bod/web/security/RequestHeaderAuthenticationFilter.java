c2b961b0edc834f980806ec2fd38002bea1af219
Hans Westerbeek <hanswesterbeek@gmail.com>
2014-03-12 16:48:57 +0100

    Retrieve oAuth token on Spring security side using a (naive) XPath expression

--- src/main/java/nl/surfnet/bod/web/security/RequestHeaderAuthenticationFilter.java
+++ src/main/java/nl/surfnet/bod/web/security/RequestHeaderAuthenticationFilter.java
@@ -24,12 +24,34 @@ package nl.surfnet.bod.web.security;
 
 import static com.google.common.base.Strings.nullToEmpty;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Resource;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Function;
@@ -45,28 +67,28 @@ public class RequestHeaderAuthenticationFilter extends AbstractPreAuthenticatedP
   private final Logger logger = LoggerFactory.getLogger(RequestHeaderAuthenticationFilter.class);
 
   private final Function<HttpServletRequest, String> imitateNameId = new Function<HttpServletRequest, String>() {
-      @Override
-      public String apply(HttpServletRequest request) {
-        String user = nullToEmpty(request.getParameter("nameId"));
-        return user.isEmpty() ? env.getImitateShibbolethUserId() : user;
-      }
-    };
+    @Override
+    public String apply(HttpServletRequest request) {
+      String user = nullToEmpty(request.getParameter("nameId"));
+      return user.isEmpty() ? env.getImitateShibbolethUserId() : user;
+    }
+  };
 
   private final Function<HttpServletRequest, String> imitateDisplayName = new Function<HttpServletRequest, String>() {
-      @Override
-      public String apply(HttpServletRequest request) {
-        String name = nullToEmpty(request.getParameter("displayName"));
-        return name.isEmpty() ? env.getImitateShibbolethDisplayName() : name;
-      }
-    };
+    @Override
+    public String apply(HttpServletRequest request) {
+      String name = nullToEmpty(request.getParameter("displayName"));
+      return name.isEmpty() ? env.getImitateShibbolethDisplayName() : name;
+    }
+  };
 
   private final Function<HttpServletRequest, String> imitateEmail = new Function<HttpServletRequest, String>() {
-      @Override
-      public String apply(HttpServletRequest request) {
-        String email = nullToEmpty(request.getParameter("email"));
-        return email.isEmpty() ? env.getImitateShibbolethEmail() : email;
-      }
-    };
+    @Override
+    public String apply(HttpServletRequest request) {
+      String email = nullToEmpty(request.getParameter("email"));
+      return email.isEmpty() ? env.getImitateShibbolethEmail() : email;
+    }
+  };
 
   @Resource(name = "bodEnvironment")
   private Environment env;
@@ -79,17 +101,68 @@ public class RequestHeaderAuthenticationFilter extends AbstractPreAuthenticatedP
     if (isNsiV1Request(request)) {
       return getPrincipalFromOauth2Header(request);
     }
+    else if(isNsiV2Request(request)) {
+      return getPrincipalFromSoapHeader(request);
+    }
     return getPrincipalFromHeaders(request);
   }
 
+  @Override
+  /**
+   * Copy the request input stream so that we may read it more than once
+   */
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+          throws IOException, ServletException {
+    super.doFilter(new CustomHttpServletRequestWrapper((HttpServletRequest) request), response, chain);
+  }
+
+
+  private Object getPrincipalFromSoapHeader(HttpServletRequest request) {
+
+    final List<String> tokensFromSoapHeader = getTokensFromSoapHeader(request);
+    for (String candidate: tokensFromSoapHeader) {
+      final RichPrincipal richPrincipal = getRichPrincipalByToken(candidate);
+      if (richPrincipal != null) {
+        return richPrincipal;
+      }
+    }
+    return null; // no token validated, so returning a null Principal
+  }
+
+  private List<String> getTokensFromSoapHeader(final HttpServletRequest request) {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+    try {
+      DocumentBuilder builder = factory.newDocumentBuilder();
+      Document doc = builder.parse(request.getInputStream());
+      XPathFactory xPathfactory = XPathFactory.newInstance();
+      XPath xpath = xPathfactory.newXPath();
+
+      XPathExpression expr = xpath.compile("//*[local-name()='Attribute']/*[local-name()='AttributeValue']/text()");
+      NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+      final ArrayList<String> result = new ArrayList<>();
+      for (int i = 0; i < nodes.getLength(); i++) {
+        Node currentItem = nodes.item(i);
+        result.add(currentItem.getTextContent().trim());
+      }
+      return result;
+    } catch (Exception e) {
+      throw new RuntimeException("Unable to find authorization tokens in soap headers", e);
+    }
+
+  }
+
+  private boolean isNsiV2Request(HttpServletRequest request) {
+    return request.getRequestURI().contains("/nsi/v2");
+  }
   private boolean isNsiV1Request(HttpServletRequest request) {
     return request.getRequestURI().contains("/nsi/v1_sc");
   }
 
   private Object getPrincipalFromHeaders(HttpServletRequest request) {
-    String nameId = getRequestHeaderOrImitate(request, ShibbolethConstants.NAME_ID, imitateNameId);
-    String displayName = getRequestHeaderOrImitate(request, ShibbolethConstants.DISPLAY_NAME, imitateDisplayName);
-    String email = getRequestHeaderOrImitate(request, ShibbolethConstants.EMAIL, imitateEmail);
+    String nameId = getRequestHeaderOrImmitate(request, ShibbolethConstants.NAME_ID, imitateNameId);
+    String displayName = getRequestHeaderOrImmitate(request, ShibbolethConstants.DISPLAY_NAME, imitateDisplayName);
+    String email = getRequestHeaderOrImmitate(request, ShibbolethConstants.EMAIL, imitateEmail);
 
     logger.info("Found Shibboleth name-id: '{}', displayName: '{}', email: {}", new Object[] {nameId, displayName, email});
 
@@ -118,22 +191,22 @@ public class RequestHeaderAuthenticationFilter extends AbstractPreAuthenticatedP
     logger.debug("Found verifiedToken {}", verifiedToken);
 
     return verifiedToken.transform(new Function<VerifiedToken, RichPrincipal>() {
-        @Override
-        public RichPrincipal apply(VerifiedToken token) {
-          return new RichPrincipal(
-              token.getPrincipal().getName(),
-              token.getPrincipal().getAttributes().get("displayName"),
-              token.getPrincipal().getAttributes().get("email"),
-              token.getScopes());
-        }
+      @Override
+      public RichPrincipal apply(VerifiedToken token) {
+        return new RichPrincipal(
+                token.getPrincipal().getName(),
+                token.getPrincipal().getAttributes().get("displayName"),
+                token.getPrincipal().getAttributes().get("email"),
+                token.getScopes());
+      }
     }).orNull();
   }
 
-  private String getRequestHeaderOrImitate(
-      HttpServletRequest request, String header, Function<HttpServletRequest, String> imitateValue) {
+  private String getRequestHeaderOrImmitate(
+          HttpServletRequest request, String header, Function<HttpServletRequest, String> immitateValue) {
     String value = nullToEmpty(request.getHeader(header));
 
-    String headerValue = value.isEmpty() && env.getImitateShibboleth() ? imitateValue.apply(request) : value;
+    String headerValue = value.isEmpty() && env.getImitateShibboleth() ? immitateValue.apply(request) : value;
 
     return new String(headerValue.getBytes(Charsets.ISO_8859_1), Charsets.UTF_8);
   }
@@ -145,5 +218,34 @@ public class RequestHeaderAuthenticationFilter extends AbstractPreAuthenticatedP
 
   protected void setEnvironment(Environment environment) {
     this.env = environment;
+  }
+
+  /**
+   * Keeps a copy of the request body
+   */
+  private static class CustomHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+    private String body;
+
+    public CustomHttpServletRequestWrapper(HttpServletRequest request) {
+      super(request);
+
+      try {
+        body = IOUtils.toString(request.getInputStream());
+      } catch (IOException exception) {
+        throw new RuntimeException("Error reading the request body", exception);
+      }
+    }
+
+    @Override
+    public ServletInputStream getInputStream () throws IOException {
+      final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
+      ServletInputStream inputStream = new ServletInputStream() {
+        public int read () throws IOException {
+          return byteArrayInputStream.read();
+        }
+      };
+      return inputStream;
+    }
   }
 }
